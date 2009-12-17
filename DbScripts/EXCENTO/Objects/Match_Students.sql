befff4ba537879c9438d93215810d993efbae83e
Peter Gindhart <peter.gindhart@vc3.com>
2009-12-17 15:48:55 -0500

    update to student matching view

--- DbScripts/EXCENTO/Objects/Match_Students.sql
+++ DbScripts/EXCENTO/Objects/Match_Students.sql
@@ -8,7 +8,28 @@ AS
 	from
 		EXCENTO.Student a join
 		dbo.Student b on
-			a.StudentID = b.Number and
+			isnull(a.Del_Flag, 0) = 0 and
+			a.SCDOE = b.x_SunsNumber
+	union
+	SELECT a.GStudentID, b.ID
+	from
+		EXCENTO.Student a join
+		dbo.Student b on
+			isnull(a.Del_Flag, 0) = 0 and
+			a.SCDOE <> b.x_SunsNumber and
+			a.StudentID = b.Number
+	union
+	SELECT a.GStudentID, b.ID
+	from
+		EXCENTO.Student a join
+		dbo.Student b on
+			isnull(a.Del_Flag, 0) = 0 and
+			a.SCDOE <> b.x_SunsNumber and
+			a.StudentID <> b.Number and
+			a.Firstname = b.FirstName and
 			a.Lastname = b.LastName and
-			a.Deletedate is null
+			a.Birthdate = b.DOB join
+		dbo.EnumValue e on
+			b.GenderID = e.ID and
+			e.DisplayValue = a.Sex
 GO
