bca534c4d97cb29768497005dda6802f63fb4e64
George Giddens <github@georgegiddens.com>
2010-02-25 09:21:01 -0500

    Transform ESY view

--- DbScripts/EXCENTO/Objects/Transform_IepEsy.sql
+++ DbScripts/EXCENTO/Objects/Transform_IepEsy.sql
@@ -5,10 +5,32 @@ GO
 CREATE VIEW [EXCENTO].[Transform_IepEsy]
 AS
 	SELECT 
-		DestID = CAST(NULL as uniqueidentifier),
-		DecisionID = CAST(NULL as uniqueidentifier),
-		TbdDate = CAST(NULL as datetime),
+		DestID = iep.DestID,
+		DecisionID = x.EnumValueId,
+		TbdDate = o.ESYDeter,
 		TbdNeededInformation = CAST(NULL as Text),
 		Explanation = CAST(NULL as Text)
-
+	FROM
+		dbo.PrgSectionType t JOIN 
+		dbo.PrgSectionDef d on t.ID = d.TypeId JOIN
+		dbo.PrgSection sec on d.ID = sec.DefId JOIN 
+		EXCENTO.Transform_Iep iep on sec.VersionID = iep.VersionDestID LEFT JOIN 
+		EXCENTO.IEPSpecialFactorTbl o ON iep.GStudentID = o.GStudentID JOIN (
+			SELECT 
+				ev.ID EnumValueID, ev.Code
+			FROM 
+				dbo.EnumType et JOIN 
+				dbo.EnumValue ev on et.ID = ev.Type
+			WHERE
+				ev.IsActive = 1 and 
+				et.Type = 'IEP.EsyDecision'
+			) X on 
+				isnull(case when Considered8 = 1 then 'Y'
+					when Considered8 = 0 then 'N'
+					when ESYDeter is not null then 'TBD'
+					else NULL
+				end, 'N') = x.Code -- IF there is no Special Factors record for the student or there is no selection for ESY, we set ESY to No
+	WHERE 
+		isnull(o.del_flag,0)=0 AND
+		t.Name = 'IEP Dates'
 GO
