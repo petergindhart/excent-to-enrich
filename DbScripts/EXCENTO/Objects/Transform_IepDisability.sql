716af3965728370bc5838a503f5735ea1d8709c7
Peter Gindhart <peter.gindhart@vc3.com>
2010-02-26 09:05:24 -0500

    Initial configuration of import of student disabilities.

---
+++ DbScripts/EXCENTO/Objects/Transform_IepDisability.sql
@@ -0,0 +1,17 @@
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[EXCENTO].[Transform_IepDisability]') AND OBJECTPROPERTY(id, N'IsView') = 1)
+	DROP VIEW [EXCENTO].[Transform_IepDisability]
+GO
+
+CREATE VIEW EXCENTO.Transform_IepDisability
+AS
+	SELECT
+		d.DisabilityID,
+		DestID = m.DestID,
+		Name = d.DisabDesc,
+		Definition = d.DisabilityID,
+		DeterminationFormTemplateID = CAST(NULL AS UNIQUEIDENTIFIER)
+	FROM
+		EXCENTO.DisabilityLook d LEFT JOIN
+		EXCENTO.MAP_IepDisabilityID m on d.DisabilityID = m.DisabilityID
+	WHERE d.StateCode IS NULL
+GO
