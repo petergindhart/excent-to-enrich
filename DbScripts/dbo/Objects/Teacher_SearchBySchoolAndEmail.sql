9ebde33c8561b6b854b88488ffc03dd46129bb86
petergindhart <peter.gindhart@vc3.com>
2010-10-26 16:59:39 -0400

    The majority of the object scripts are generated directly from the development database.  Rather than keep these in source control, simply maintain an archive of the current object scripts.

--- DbScripts/dbo/Objects/Teacher_SearchBySchoolAndEmail.sql
+++
@@ -1,3 +0,0 @@
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[Teacher_SearchBySchoolAndEmail]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
-drop procedure [dbo].[Teacher_SearchBySchoolAndEmail]
-GO
--- DbScripts/dbo/Objects/z_RepublishReports.sql
+++
@@ -1,3 +0,0 @@
-
--- Force all reports to be republished in case RDL templates have changed
-update Report set IsPublished = 0
