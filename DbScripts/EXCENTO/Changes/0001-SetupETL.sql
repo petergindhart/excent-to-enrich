9fca296ff5bc042c432771be061bed7e741d8169
unknown <georgeg@eorgegiddens.com>
2011-05-06 08:25:06 -0600

    Clean up EXCENTO file deletions.

--- DbScripts/EXCENTO/Changes/0001-SetupETL.sql
+++
@@ -1,5 +0,0 @@
---#assume VC3ETL:20
-
--- #############################################################################
--- ExtractDatabase Configuration
-INSERT INTO VC3ETL.ExtractDatabase VALUES ( '9756E9BB-8B6B-44E4-9C4E-B3F8E6A6CD16', 'ACBEF25A-A8EB-465B-97D8-9738F07C3023', 'CBE6E716-95F0-44BC-837C-BBC4FD59506C', NULL, NULL, 'Richland1_ExcentOnline', 'username', 'password', NULL, 0, NULL, NULL, NULL, '[{BrandName}] {SisDatabase} import completed', 'Successfully imported {SisDatabase} data into {BrandName}.  {SisDatabase} data in {BrandName} is now current as of {SnapshotDate}.', NULL, '[{BrandName}] {SisDatabase} import failed', 'There was a problem importing {SisDatabase} data into {BrandName}:  {ErrorMessage}', 1, '_NEW', '_LOCAL', NULL, NULL, 'Excent Online', 0)
--- DbScripts/EXCENTO/Objects/clear_pending_tasks.sql
+++
@@ -1,4 +0,0 @@
--- cancel any pending or 'running' tasks
-update VC3TaskScheduler.ScheduledTask set StatusID = 'C' where StatusID in ('P', 'R')
--- ensure no scheduled tasks will initiate
-update VC3TaskScheduler.ScheduledTaskSchedule set LastRunTime = DATEADD(YEAR, 1, GETDATE()) where IsEnabled = 1
