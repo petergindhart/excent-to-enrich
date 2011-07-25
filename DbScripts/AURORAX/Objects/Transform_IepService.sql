b9daef61d4f7697053c3909915b328d68503190a
petergindhart <peter.gindhart@vc3.com>
2011-07-25 09:29:49 -0400

    add ServiceProviderTitle / IepServiceCategory reference

--- DbScripts/AURORAX/Objects/Transform_IepService.sql
+++ DbScripts/AURORAX/Objects/Transform_IepService.sql
@@ -5,19 +5,12 @@ GO
 create view AURORAX.Transform_IepService
 as
 select
-	v.ServiceType,
 	v.ServiceRefId,
 	iep.IepRefId,
 	m.DestID,
--- IepServicePlan.  The case statements assume source data indicates Y or N (this is validated in the data validation tool)
-	InstanceID = sec.ID,
-	RelatedID = case when IsRelated = 'Y' then '4CA5DB1F-2CAC-4DDC-B856-B4B8BFE88BDD' else '4570E6F2-2691-4BB1-9BBB-A62AC3BEECB7' end, -- 4570E6F2-2691-4BB1-9BBB-A62AC3BEECB7	Special Education, 4CA5DB1F-2CAC-4DDC-B856-B4B8BFE88BDD	Related
-	DirectID = case when IsDirect = 'Y' then 'A7061714-ADA3-44F7-8329-159DD4AE2ECE' else '1A8BF908-E3ED-45B0-8EEC-99CB1AD0806F' end, -- A7061714-ADA3-44F7-8329-159DD4AE2ECE	Direct, 1A8BF908-E3ED-45B0-8EEC-99CB1AD0806F	Indirect
-	ExcludesID = case when ExcludesFromGenEd = 'Y' then '493713FB-6071-42D4-B46A-1B09037C1F8B' else '235C3167-A3E6-4D1D-8AAB-0B2B57FD5160' end, -- 235C3167-A3E6-4D1D-8AAB-0B2B57FD5160	Inside, 493713FB-6071-42D4-B46A-1B09037C1F8B	Outside
-	EsyID = case when IsEsy = 'Y' then 'B76DDCD6-B261-4D46-A98E-857B0A814A0C' else 'F7E20A86-2709-4170-9810-15B601C61B79' end, -- F7E20A86-2709-4170-9810-15B601C61B79	N, B76DDCD6-B261-4D46-A98E-857B0A814A0C	Y
-	DeliveryStatement = cast(NULL as text),
 -- ServicePlan
 	DefId = sdm.DestId,
+	InstanceID = sec.ID,
 	iep.StudentID,
 	StartDate = cast(v.BeginDate as datetime),
 	EndDate = cast(v.EndDate as datetime),
@@ -31,7 +24,14 @@ select
 		WHERE IepRefID = v.IepRefID AND
 		ServiceRefID < v.ServiceRefID
 		),
-   ServiceTypeID = 'D3945E9D-AA0E-4555-BCB2-F8CA95CC7784',
+	ServiceTypeID = 'D3945E9D-AA0E-4555-BCB2-F8CA95CC7784',
+-- IepServicePlan.  The case statements assume source data indicates Y or N (this is validated in the data validation tool)
+	CategoryCode = v.ServiceType,
+	CategoryID = cat.DestID,
+	DirectID = case when IsDirect = 'Y' then 'A7061714-ADA3-44F7-8329-159DD4AE2ECE' else '1A8BF908-E3ED-45B0-8EEC-99CB1AD0806F' end, -- A7061714-ADA3-44F7-8329-159DD4AE2ECE	Direct, 1A8BF908-E3ED-45B0-8EEC-99CB1AD0806F	Indirect
+	ExcludesID = case when ExcludesFromGenEd = 'Y' then '493713FB-6071-42D4-B46A-1B09037C1F8B' else '235C3167-A3E6-4D1D-8AAB-0B2B57FD5160' end, -- 235C3167-A3E6-4D1D-8AAB-0B2B57FD5160	Inside, 493713FB-6071-42D4-B46A-1B09037C1F8B	Outside
+	EsyID = case when IsEsy = 'Y' then 'B76DDCD6-B261-4D46-A98E-857B0A814A0C' else 'F7E20A86-2709-4170-9810-15B601C61B79' end, -- F7E20A86-2709-4170-9810-15B601C61B79	N, B76DDCD6-B261-4D46-A98E-857B0A814A0C	Y
+	DeliveryStatement = cast(NULL as text),
 -- ServiceSchedule
 	ServiceSchedueDestID = ssm.DestID,
 	ProviderID = cast(NULL as uniqueidentifier),  -- will require a matching view similar to Match_Students, but is complicated by how the UserProfile works
@@ -49,6 +49,7 @@ FROM
 	AURORAX.MAP_ServiceLocationID loc on v.ServiceLocationCode = loc.ServiceLocationCode LEFT JOIN 
 	AURORAX.MAP_ServiceFrequencyID freq on v.ServiceFrequencyCode = freq.ServiceFrequencyCode LEFT JOIN 
  	AURORAX.MAP_ServiceProviderTitleID ttl on v.ServiceProviderTitleCode = ttl.ServiceProviderTitleCode LEFT JOIN
+ 	AURORAX.MAP_IepServiceCategoryID cat on v.ServiceType = cat.SubType LEFT JOIN
 	AURORAX.MAP_ScheduleID ssm on v.ServiceRefID = ssm.ServiceRefID
 GO
 ---
