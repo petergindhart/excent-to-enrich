19722f2bf2b911c81a2dc11fc2df05eeec94a9b8
unknown <georgeg@eorgegiddens.com>
2011-08-01 04:58:22 -0600

    Joining LRE on StateCode (it so happens they are the same for this district). StateCode was updated in Template

--- DbScripts/AURORAX/Objects/Transform_LREPlacement.sql
+++ DbScripts/AURORAX/Objects/Transform_LREPlacement.sql
@@ -14,12 +14,10 @@ AS
 		IsDecOneCount = 0
 	FROM
 		AURORAX.Transform_Iep iep LEFT JOIN -- left join for speed (filter in where clause)
-		AURORAX.Transform_Section sec ON sec.ItemID = iep.DestID JOIN -- and sec.DefID = '0CBA436F-8043-4D22-8F3D-289E057F1AAB' JOIN
+		AURORAX.Transform_Section sec ON sec.ItemID = iep.DestID JOIN
 		AURORAX.Transform_LRE lre ON lre.DestID = sec.DestID JOIN
 		AURORAX.IEP lre01 ON lre01.IepRefID = iep.IepRefID JOIN
---		AURORAX.Map_PlacementOptionID placeMap ON lre01.LRECode = placeMap.LRECode LEFT JOIN
--- 	AURORAX.Map_PlacementOptionID placeMap ON lre.LRECode = placeMap.LRECode LEFT JOIN
-		dbo.IepPlacementOption o on lre01.LRECode = o.StateCode join
+		dbo.IepPlacementOption o on lre01.LRECode = o.StateCode left join														--- LRE STATE CODE
 		dbo.IepPlacement place ON lre.DestID = place.ID
 	WHERE
 		sec.DefID = '0CBA436F-8043-4D22-8F3D-289E057F1AAB'
