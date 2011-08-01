81476829cd1a4acf87b6483b9901406481b5aa60
unknown <georgeg@eorgegiddens.com>
2011-08-01 04:59:32 -0600

    Added available columns to select statement and added ServFreq MAP.

--- DbScripts/AURORAX/Objects/Transform_Schedule.sql
+++ DbScripts/AURORAX/Objects/Transform_Schedule.sql
@@ -8,12 +8,12 @@ AS
   v.ServiceRefID,
   m.DestID,
   TypeID = 'A27EC71B-2F61-46D5-A371-3F92C7642AEF',   
-  StartDate = cast(NULL as datetime),   
-  EndDate = cast(NULL as datetime),   
+  StartDate = cast(v.BeginDate as datetime),
+  EndDate = cast(v.EndDate as datetime),   
   LastOccurrence = cast(NULL as datetime),   
   IsEnabled = cast(0 as bit),   
-  FrequencyID = cast(NULL as uniqueidentifier),
-  FrequencyAmount = cast(0 as int),   
+  FrequencyID = mf.DestID,
+  FrequencyAmount = cast(v.ServiceTime as int),   
   WeeklyMon = cast(0 as bit),   
   WeeklyTue = cast(0 as bit),   
   WeeklyWed = cast(0 as bit),   
@@ -25,8 +25,7 @@ AS
  FROM  
   AURORAX.Service v LEFT JOIN
   AURORAX.MAP_ScheduleID m on v.ServiceRefID = m.ServiceRefID LEFT JOIN
+  AURORAX.Map_ScheduleFrequencyID mf on v.ServiceFrequencyCode = mf.ServiceFrequencyCode left join
   dbo.Schedule sched on m.DestID = sched.ID
 GO  
 --
-
--- note to self:  Use Transform_Service instead and use the actual Frequency ID!!!
