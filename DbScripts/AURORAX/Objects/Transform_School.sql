be84e70ea332d463f2243402b67c4b9feee7cc88
unknown <georgeg@eorgegiddens.com>
2011-08-01 05:00:20 -0600

    Modified with intent to leave SIS schools alone.

--- DbScripts/AURORAX/Objects/Transform_School.sql
+++ DbScripts/AURORAX/Objects/Transform_School.sql
@@ -5,10 +5,10 @@ GO
 CREATE VIEW [AURORAX].[Transform_School]
 AS
 /*
-  Note:  we are not using a map and we will not be deleting any existing records.
-  Using ManuallyEntered = 1 filter to ensure we don't touch any records maintained by SIS imports
-
+	This view will only return rows if there is more than one OrgUnit in the OrgUnit table
+	
 */
+
 select
 	src.SchoolRefID,
 	src.SchoolCode,
@@ -29,11 +29,14 @@ select
 from 
 	AURORAX.School src JOIN
 	AURORAX.MAP_OrgUnit mo on src.DistrictRefID = mo.DistrictRefID LEFT JOIN
-	AURORAX.MAP_SchoolRefID m on src.SchoolRefID = m.SchoolRefID
-
+	AURORAX.MAP_SchoolRefID m on src.SchoolRefID = m.SchoolRefID -- LEFT JOIN
+-- 	School tgt on o.ID = tgt.OrgUnitID and src.SchoolCode = tgt.Number
 GO
+
+
 
 
 IF  EXISTS (SELECT 1 from sys.schemas s join sys.objects o on s.schema_id = o.schema_id where s.name = 'AURORAX' and o.name = 'MAP_SchoolView')
 	DROP VIEW AURORAX.MAP_SchoolView
 GO
+--
