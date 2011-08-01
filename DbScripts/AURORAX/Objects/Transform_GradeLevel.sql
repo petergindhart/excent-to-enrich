0862239be67cede08aea8872d61a6416b4e6c093
unknown <georgeg@eorgegiddens.com>
2011-08-01 04:55:44 -0600

    Added StateCode to Template and joined on StateCode

--- DbScripts/AURORAX/Objects/Transform_GradeLevel.sql
+++ DbScripts/AURORAX/Objects/Transform_GradeLevel.sql
@@ -6,11 +6,17 @@ CREATE VIEW [AURORAX].[Transform_GradeLevel]
 AS
 	SELECT
 		DestID = ISNULL(x.ID, NEWID()),
-		Name = l.Code
+		Name = l.Code,
+		StateCode = l.StateCode
 	FROM
 		AURORAX.Lookups l LEFT JOIN
-		GradeLevel x on l.Code = x.Name
+		GradeLevel x on l.StateCode = x.StateCode -- since there is no map table we are going to have to provide the state code in the source data.
 	WHERE
 		l.Type = 'Grade'
 GO
 --
+
+-- select * from GradeLevel order by sequence -- select * from [AURORAX].[Transform_GradeLevel] order by name
+
+-- delete GradeLevel where Name = '002'
+
