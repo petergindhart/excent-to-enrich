4668adc173e5d15436ccef3c0d06f464e4c776bf
unknown <georgeg@eorgegiddens.com>
2011-09-02 10:50:03 -0600

    Modified for consistant lookup ETL that is designed to work in any state and any district.

--- DbScripts/AURORAX/Objects/Transform_Student.sql
+++ DbScripts/AURORAX/Objects/Transform_Student.sql
@@ -1,23 +1,45 @@
 --#include Transform_School.sql
---#include Transform_Ethnicity.sql
+
+-- #############################################################################
+-- Student
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'AURORAX.MAP_StudentRefID') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE AURORAX.MAP_StudentRefID
+	(
+	StudentRefID varchar(150) NOT NULL,
+	LegacyData bit not null,
+	DestID uniqueidentifier NOT NULL
+	)  
+
+ALTER TABLE AURORAX.MAP_StudentRefID ADD CONSTRAINT
+	PK_MAP_StudentRefID PRIMARY KEY CLUSTERED
+	(
+	StudentRefID
+	) 
+END
+GO
+
+
 IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[AURORAX].[Transform_Student]') AND OBJECTPROPERTY(id, N'IsView') = 1)
 DROP VIEW [AURORAX].[Transform_Student]
 GO
 
 CREATE VIEW [AURORAX].[Transform_Student]
 AS
+-- NOTE:  DO NOT TOUCH THE RECORDS ADDED BY SIS IMPORT OR MANUALLY ENTERED STUDENTS.  SIS RECORDS DO NEED TO BE MAPPED.  NEW RECORDS FROM SPED NEED TO BE ADDED. 
  SELECT
   src.StudentRefID,
-  DestID = isnull(dest.ID, ms.DestID), -- it seems I keep changing this.  may need to use isnull
+  DestID = coalesce(s.ID, t.ID, m.DestID),
+  LegacyData = ISNULL(m.LegacyData, case when s.ID IS NULL then 1 else 0 end), -- allows updating only legacy data by adding a DestFilter in LoadTable.  Leaves real ManuallyEntered students untouched.
   CurrentSchoolID = sch.DestID,  
-  CurrentGradeLevelID = g.ID,  
-  EthnicityID = te.DestID,
+  CurrentGradeLevelID = g.DestID,  
+  EthnicityID = CAST(NULL as uniqueidentifier),
   GenderID = (select ID from EnumValue where Type = 'D6194389-17AC-494C-9C37-FD911DA2DD4B' and Code = src.Sex), -- will error if more than one value
   Number = src.StudentLocalID,  
   src.FirstName,  
   src.MiddleName,  
   src.LastName,  
-  SSN = cast(null as varchar),  
+  SSN = cast(null as varchar),
   DOB = src.Birthdate,
   Street = cast(null as varchar),  
   City = cast(null as varchar),
@@ -42,20 +64,89 @@ AS
   x_DisabilityType = cast(NULL as uniqueidentifier),  
   x_FreeLunchID = cast(NULL as uniqueidentifier),  
   x_CSAP_A = cast(0 as bit),  
-  ManuallyEntered = cast(isnull(dest.ManuallyEntered,1) as bit), -- cast(case when dest.ID is null then 1 else 0 end as bit),  
-  IsActive = cast(isnull(dest.IsActive,1) as bit),  
   IsHispanic = case when src.IsHispanic = 'Y' then 1 else 0 end,  
   ImportPausedDate = cast(NULL as datetime),  
-  ImportPausedByID = cast(NULL as uniqueidentifier)  
- FROM -- NOTE:  DO NOT TOUCH THE RECORDS ADDED BY SIS IMPORT.  SIS RECORDS DO NEED TO BE MAPPED.  NEW RECORDS FROM SPED NEED TO BE ADDED. 
-  AURORAX.Student src LEFT JOIN  
-  AURORAX.Transform_Ethnicity te on src.EthnicityCode = te.Code JOIN
-  dbo.GradeLevel g on src.GradeLevelCode = g.StateCode JOIN							-- we have state code in the GradeLevel now
-  AURORAX.MAP_SchoolRefID sch on src.ServiceSchoolRefID = sch.SchoolRefID LEFT JOIN  
-  AURORAX.MAP_StudentRefID ms on src.StudentRefID = ms.StudentRefID LEFT JOIN  
-  dbo.Student dest on src.StudentLocalID = dest.Number and src.LastName = dest.LastName
- WHERE 
-  dest.IsActive = 1 -- 10721
+  ImportPausedByID = cast(NULL as uniqueidentifier),  -- select src.StudentLocalID
+  IsActive = cast(isnull(t.IsActive,1) as bit),  
+  ManuallyEntered = cast(isnull(t.ManuallyEntered,1) as bit) -- cast(case when dest.ID is null then 1 else 0 end as bit),  
+ FROM 
+  AURORAX.Student src LEFT JOIN
+  -- AURORAX.Transform_Ethnicity te on src.EthnicityCode = te.Code LEFT JOIN
+  AURORAX.Transform_GradeLevel g on src.GradeLevelCode = g.GradeLevelCode LEFT JOIN
+  AURORAX.Transform_School sch on src.ServiceSchoolRefID = sch.SchoolRefID LEFT JOIN
+  dbo.Student s on src.StudentLocalID = s.Number and s.IsActive = 1 LEFT JOIN -- identifies students in legacy data that match students in Enrich
+  AURORAX.MAP_StudentRefID m on src.StudentRefID = m.StudentRefID LEFT JOIN
+  dbo.Student t on m.DestID = t.ID -- exists in map table
+ --WHERE
+ -- t.ID is null   -- not in Enrich yet.  Do not use this where clause.  we want to see students added by SIS and Legacy Sped students at the same time
 GO
--- select count(*) from [AURORAX].[Transform_Student] -- 11091
+
+
+/*
+
+NOTES: 
+
+Import strategy:  
+	don't touch sis data (no updates or deletes)
+	delete missing map records
+		legacy student data remains in dbo.Student
+	update only legacy student records
+	insert legacy student records that don't exist in dbo.Student 
+
+The mechanism to distinguish Legacy students from SIS students is the AURORAX.MAP_StudentRefID.LegacyData.  Note how this is derived in the Transform query
+
+
+
+GEO.ShowLoadTables Student
+
+set nocount on;
+declare @n varchar(100) ; select @n = 'Student'
+declare @t uniqueidentifier ; select @t = id from VC3ETL.LoadTable where ExtractDatabase = '29D14961-928D-4BEE-9025-238496D144C6' and DestTable = @n
+update t set 
+	HasMapTable = 1
+	, MapTable = 'AURORAX.MAP_StudentRefID'
+	, KeyField = 'StudentRefID, LegacyData'
+	, DeleteKey = 'DestID'
+	, DeleteTrans = 1
+	, UpdateTrans = 1
+	, DestTableFilter = 's.LegacyData = 1'
+	, Enabled = 1
+	from VC3ETL.LoadTable t where t.ID = @t
+exec VC3ETL.LoadTable_Run @t, '', 1, 0
+print '
+
+select * from '+@n
+
+select d.*
+-- DELETE AURORAX.MAP_StudentRefID
+FROM AURORAX.Transform_Student AS s RIGHT OUTER JOIN 
+	AURORAX.MAP_StudentRefID as d ON s.DestID = d.DestID
+WHERE (s.DestID IS NULL)
+
+select d.*
+-- UPDATE Student SET CurrentGradeLevelID=s.CurrentGradeLevelID, CurrentSchoolID=s.CurrentSchoolID, Number=s.Number, MiddleName=s.MiddleName, GenderID=s.GenderID, LastName=s.LastName, ManuallyEntered=s.ManuallyEntered, DOB=s.DOB, IsActive=s.IsActive, FirstName=s.FirstName, IsHispanic=s.IsHispanic
+FROM  Student d JOIN 
+	AURORAX.Transform_Student  s ON s.DestID=d.ID
+	AND s.LegacyData = 1
+
+-- INSERT AURORAX.MAP_StudentRefID
+SELECT StudentRefID, LegacyData, NEWID()
+FROM AURORAX.Transform_Student s
+WHERE NOT EXISTS (SELECT * FROM Student d WHERE s.DestID=d.ID)
+
+-- INSERT Student (ID, CurrentGradeLevelID, CurrentSchoolID, Number, MiddleName, GenderID, LastName, ManuallyEntered, DOB, IsActive, FirstName, IsHispanic)
+SELECT s.DestID, s.CurrentGradeLevelID, s.CurrentSchoolID, s.Number, s.MiddleName, s.GenderID, s.LastName, s.ManuallyEntered, s.DOB, s.IsActive, s.FirstName, s.IsHispanic
+FROM AURORAX.Transform_Student s
+WHERE NOT EXISTS (SELECT * FROM Student d WHERE s.DestID=d.ID)
+
+
+select * from Student where ManuallyEntered = 1 -- students that really were manually entered plus students added by ETL
+
+
+select * from AURORAX.MAP_StudentRefID 
+
+
+
+*/
+
 
