3453254eb57de0dfbd2d0e1ed1e01ecce4766efe
unknown <georgeg@eorgegiddens.com>
2011-09-02 15:13:42 -0600

    new file for separating district-specific ETL code from common code. This is the file to put district-specific customizations into ETL.

---
+++ DbScripts/AURORAX/Objects/0001c-ETLPrep_District_Lee.sql
@@ -0,0 +1,47 @@
+--#include ..\Objects\Transform_ServiceFrequency.sql
+
+-- Map_ServiceFrequencyID is created in the Transform script.
+
+/*
+
+	In an effort to limit the number of files that needs to be touched for each new implementation of Enrich,
+	this file contains all district-specific ETL connfiguration.
+*/
+
+-- OrgUnit 
+update ou set Number = '36' -- Lee County State Reporting DistrictID
+-- select ou.*
+from dbo.OrgUnit ou join 
+	dbo.SystemSettings ss on ou.ID = ss.LocalOrgRootID 
+go
+
+
+/*
+	ServiceFrequency is part of seed data in Enrich.  Thus it must be hard-mapped.  
+	ServiceFrequency did not support hiding from UI at the time this code was written, so additional service frequencies are not supported.
+		For additional frequencies it may be possible to calculate the frequency based on an existing value 
+			i.e. 2 times Quarterly = 8 times yearly,  30 minutes per quarter = 2 hours per year or 120 minutes per year
+*/
+declare @Map_ServiceFrequencyID table (ServiceFrequencyCode varchar(30), ServiceFrequencyName varchar(50), DestID uniqueidentifier)
+set nocount on;
+insert @Map_ServiceFrequencyID values ('day', 'daily', '71590A00-2C40-40FF-ABD9-E73B09AF46A1')
+insert @Map_ServiceFrequencyID values ('week', 'weekly', 'A2080478-1A03-4928-905B-ED25DEC259E6')
+insert @Map_ServiceFrequencyID values ('month', 'monthly', '3D4B557B-0C2E-4A41-9410-BA331F1D20DD')
+insert @Map_ServiceFrequencyID values ('year', 'yearly', '5F3A2822-56F3-49DA-9592-F604B0F202C3')
+insert @Map_ServiceFrequencyID values ('unknown', 'unknown', 'C42C50ED-863B-44B8-BF68-B377C8B0FA95')
+
+
+set nocount off;
+insert AURORAX.Map_ServiceFrequencyID
+select m.*
+from @Map_ServiceFrequencyID m left join
+	AURORAX.MAP_ServiceFrequencyID t on m.DestID = t.DestID
+where t.DestID is null
+
+insert ServiceFrequency (ID, Name, Sequence, WeekFactor)
+select DestID, m.ServiceFrequencyName, 99, 0
+from AURORAX.MAP_ServiceFrequencyID m left join
+	ServiceFrequency t on m.DestID = t.ID
+where t.ID is null
+GO
+
