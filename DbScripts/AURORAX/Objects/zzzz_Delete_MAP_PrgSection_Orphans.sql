8777c8a568d6f742992f6beecadfb4cf884e3bab
unknown <georgeg@eorgegiddens.com>
2011-09-08 09:24:29 -0600

    move delete orphans to map rename scripts and delete orphan delete proc (experiment)

--- DbScripts/AURORAX/Objects/zzzz_Delete_MAP_PrgSection_Orphans.sql
+++
@@ -1,6 +0,0 @@
-
-delete m
-from AURORAX.MAP_PrgSectionID m 
-left join PrgSection s on m.DestID = s.ID
-where s.ID is null
-GO
--- DbScripts/AURORAX/Objects/zzzz_Delete_MAP_PrgVersion_Orphans.sql
+++
@@ -1,5 +0,0 @@
-delete m 
-from AURORAX.MAP_PrgVersionID m left join 
-	dbo.PrgVersion v on m.DestID = v.ID
-where v.ID is null
-go
