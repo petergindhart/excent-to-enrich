38d02d7f6146d7c7c75eb4eb98705d4424ad5977
unknown <georgeg@eorgegiddens.com>
2011-09-08 06:18:10 -0600

    Minor edit

--- DbScripts/AURORAX/Objects/Transform_PrgStatus.sql
+++ DbScripts/AURORAX/Objects/Transform_PrgStatus.sql
@@ -61,6 +61,12 @@ GO
 
 /*
 
+
+select * from PrgStatus where ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' and IsExit = 1  order by sequence
+
+
+
+
 GEO.ShowLoadTables PrgStatus
 
 set nocount on;
