8e869d89378f6bb8b15be5473579b61c91f16cd4
unknown <georgeg@eorgegiddens.com>
2011-09-23 14:12:03 -0600

    There is no error viewing Programs or IEP when there are no goals.

--- DbScripts/LEGACYSPED/Objects/Transform_PrgGoals.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_PrgGoals.sql
@@ -13,7 +13,8 @@ AS
 	PrgSection sec ON
 		sec.VersionID = iep.VersionDestID AND -- our map of PrgSection is using ItemID instead of VersionID.  Does that matter?
 		sec.DefID = '84E5A67D-CC9A-4D5B-A7B8-C04E8C3B8E0A' --IEP Goals
-	WHERE exists (select 1 from LEGACYSPED.Goal g where iep.IepRefID = g.IepRefID) -- 10715 (interpretation : 6 ieps do not have goals.  Do not insert a PrgGoals record for these)
+	--WHERE exists (select 1 from LEGACYSPED.Goal g where iep.IepRefID = g.IepRefID) -- 10715 (interpretation : 6 ieps do not have goals.  Do not insert a PrgGoals record for these)
+	-- Pete recommended to remove this.  Test before checking in the code (there is a PrgSection record for Goals)
 GO
 -- 
 /*
