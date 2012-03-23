7addae2e4d8a63d4336cad76bcd72614bdc37201
muralikrishna0112 <krishna150866@gmail.com>
2012-03-23 10:39:07 -0600

    commit

--- Support/upgrade_db_ST_District.bat
+++ Support/upgrade_db_ST_District.bat
@@ -1,8 +1,8 @@
-set state=YourState
-set speddistrict=YourDistrict
+set state=CO
+set speddistrict=Aurora
 
 echo DB Upgrade %state% %speddistrict%
 
-set connection=YourConnectionString
+set connection=''
 
 ExecuteTask\ExecuteTask.exe project projectfile="upgrade_db_project.xml" $connection="%connection%" $state="%state%" $speddistrict="%speddistrict%"
