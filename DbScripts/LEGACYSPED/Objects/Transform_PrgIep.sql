a4ac4047175a31659f39bb6816f0888759061e31
muralikrishna0112 <krishna150866@gmail.com>
2012-03-26 08:48:00 -0600

    commite20120326

--- DbScripts/LEGACYSPED/Objects/Transform_PrgIep.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_PrgIep.sql
@@ -71,7 +71,7 @@ AS
 		GradeLevelID = stu.CurrentGradeLevelID,
 		InvolvementID = inv.DestID,
 		StartStatusID =  '796C212F-6003-4CD3-878D-53BEBE087E9A', -- def.StatusID, -- Converted IEP is a soft-deleted PrgStatus record that we use by default.  Update TEMPLATE PrgItemDef.StatusID for Conveted IEP if the customer requests it
-		EndStatusID = case when (isnull(CONVERT(datetime, iep.IEPEndDate), 0) < getdate() and stu.SpecialEdStatus = 'I') then '12086FE0-B509-4F9F-ABD0-569681C59EE2' else NULL end, -- select * from PrgStatus where ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' and IsExit = 1 and sequence < 99
+		--EndStatusID = case when (isnull(CONVERT(datetime, iep.IEPEndDate), 0) < getdate() and stu.SpecialEdStatus = 'I') then '12086FE0-B509-4F9F-ABD0-569681C59EE2' else NULL end, -- select * from PrgStatus where ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' and IsExit = 1 and sequence < 99
 		PlannedEndDate = isnull(convert(datetime, iep.IEPEndDate), dateadd(yy, 1, dateadd(dd, -1, convert(datetime, iep.IEPStartDate)))),
 		-- IsEnded = case when isnull(CONVERT(datetime, iep.IEPEndDate), dateadd(yy, 1, dateadd(dd, -1, convert(datetime, iep.IEPStartDate)))) < getdate() then 1 else 0 end, -- dateadd(yy, 1, dateadd(dd, -1, convert(datetime, iep.IEPStartDate)))
 		IsEnded = cast(case when stu.SpecialEdStatus = 'I' then 1 else 0 end as Bit),
