856ecdb61aeeb27d8c1f1a8920983231b13c2dab
George Giddens <github@georgegiddens.com>
2012-03-29 11:32:36 -0400

    changed "if exists" syntax.

--- DbScripts/LEGACYSPED/Changes/0009-Extract-StudDisability.sql
+++ DbScripts/LEGACYSPED/Changes/0009-Extract-StudDisability.sql
@@ -1,7 +1,7 @@
 -- If and when a specific disabilities file is imported from APS, we will use this file for the LOCAL table definition.
 -- Until then, the disability information is in the Student.csv file
 
-IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'LEGACYSPED.StudentDisability') AND OBJECTPROPERTY(id, N'IsUserView') = 1)
+IF  EXISTS (SELECT 1 FROM sys.schemas s join sys.objects o on s.schema_id = o.schema_id WHERE s.name = 'LEGACYSPED' and o.name = 'StudentDisability' and o.type = 'V') -- select * from LEGACYSPED.StudentDisability
 	DROP VIEW LEGACYSPED.StudentDisability
 GO
 
