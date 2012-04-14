50a37142d1735d354455bf500b5a347f96fdb0ce
George Giddens <github@georgegiddens.com>
2012-04-14 11:37:38 -0400

    git is prompting me to commit this.  this is an insignificant file.

--- Support/upgrade_db_VERSION.bat
+++ Support/upgrade_db_VERSION.bat
@@ -1,4 +1,4 @@
-set modulesdirectory="C:\inetpub\Sites\Enrich\Excent Enrich DC7 FL Lee\Setup\7.20.2.2820\Product\DbScripts"
+set modulesdirectory="E:\Sites\Enrich\Excent Enrich DC7 FL Lee\Setup\7.24.2.3262\Product\DbScripts"
 set connection="server=.;uid=DC7_FL_Lee_User;pwd=vc3go!!;database=Enrich_DC7_FL_Lee"
 
 ExecuteTask\ExecuteTask.exe upgradedb modulesdirectory=%modulesdirectory% connectionstring=%connection% ignoremissingmodules=true
