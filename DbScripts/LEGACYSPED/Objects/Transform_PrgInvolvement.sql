52e2103734bce5974a0b513f117a3b10e31db0f2
George Giddens <github@georgegiddens.com>
2012-04-27 00:13:39 -0400

    Significant changes to this transform for performance and to handle new cases. Left a large number of comments to aid continued development, as this view is not complete.

--- DbScripts/LEGACYSPED/Objects/Transform_PrgInvolvement.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_PrgInvolvement.sql
@@ -29,27 +29,90 @@ CREATE VIEW LEGACYSPED.Transform_PrgInvolvement
 AS
 	SELECT
 		StudentRefID = stu.StudentRefID,
-		DestID = coalesce(x.ID, t.ID, m.DestID),
+		DestID = max(isnull(
+						isnull(
+							convert(varchar(36), x.ID), 
+							convert(varchar(36), t.ID)
+							), 
+							convert(varchar(36), m.DestID)
+						)
+					),
 		StudentID = stu.DestID,
 		ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C',   -- Special Education
 		VariantID = '6DD95EA1-A265-4E04-8EE9-78AE04B5DB9A',   -- Special Education
 		StartDate = min(iep.IEPStartDate),   -- school start for this IEP period
-		EndDate = min(case when stu.SpecialEdStatus = 'I' then iep.IEPEndDate else NULL end),
+		EndDate = min(isnull(t.EndDate, case when stu.SpecialEdStatus = 'I' then iep.IEPEndDate else NULL end)),
 		EndStatusID = min(case when stu.SpecialEdStatus = 'I' then '12086FE0-B509-4F9F-ABD0-569681C59EE2' else NULL end),
-		IsManuallyEnded = min(cast(case when stu.SpecialEdStatus = 'I' then 1 else 0 end as tinyint))
+		IsManuallyEnded = min(cast(case when stu.SpecialEdStatus = 'I' then 1 else 0 end as tinyint)),
+		Touched = min(isnull(cast(t.IsManuallyEnded as int),0))
 	FROM
 		LEGACYSPED.Transform_Student stu JOIN 
 		LEGACYSPED.IEP iep on stu.StudentRefID = iep.StudentRefID LEFT JOIN 
-		dbo.PrgInvolvement x on stu.DestID = x.StudentID and x.ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' left join 
+		dbo.PrgInvolvement x on stu.DestID = x.StudentID and x.ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' and dbo.DateInRange( iep.IEPStartDate, x.StartDate, x.EndDate ) = 1 left join 
 		LEGACYSPED.MAP_PrgInvolvementID m on iep.StudentRefID = m.StudentRefID LEFT JOIN
 		-- identify students that already have a sped invovlement that will overlap with this involvement
-		dbo.PrgInvolvement t on 
-			(stu.DestID = t.StudentID and 
-			t.ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' and
+		dbo.PrgInvolvement t on m.DestID = t.ID
+			--(stu.DestID = t.StudentID and 
+			--t.ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' 
+			--and
 		--	dbo.DateRangesOverlap (iep.IEPStartDate, iep.IEPEndDate, t.StartDate, t.EndDate, null) = 1)
-			dbo.DateInRange( iep.IEPStartDate, t.StartDate, t.EndDate ) = 1)
+		-- 	dbo.DateInRange( iep.IEPStartDate, t.StartDate, t.EndDate ) = 1)
 	WHERE 
-		iep.IEPStartDate is not null
+		iep.IEPStartDate is not null 
+	GROUP BY stu.StudentRefID, stu.DestID
+GO
+--
+
+-- set transaction isolation level read uncommitted
+
+
+-- select * from legacysped.transform_prginvolvement where studentrefid = '84689AAE-F4B1-4EA5-8732-0C95DE51EBB5'
+
+
+
+
+
+--select mstu.StudentRefID, mDestID = min(coalesce(convert(varchar(36), mx.ID), convert(varchar(36), mt.ID), convert(varchar(36), mm.DestID)))
+--from LEGACYSPED.Transform_Student mstu join 
+--	LEGACYSPED.IEP miep on mstu.StudentRefID = miep.StudentRefID join
+--	dbo.PrgInvolvement mx on mstu.DestID = mx.StudentID and mx.ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' left join 
+--	LEGACYSPED.MAP_PrgInvolvementID mm on miep.StudentRefID = mm.StudentRefID left join
+--		dbo.PrgInvolvement mt on 
+--			(mstu.DestID = mt.StudentID and 
+--			mt.ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' and
+--			dbo.DateInRange( miep.IEPStartDate, mt.StartDate, mt.EndDate ) = 1)
+--group by mstu.StudentRefID
+
+
+--select mDestID = min(coalesce(convert(varchar(36), mx.ID), convert(varchar(36), mt.ID), convert(varchar(36), mm.DestID)))
+--FROM
+--	LEGACYSPED.Transform_Student mstu JOIN 
+--	LEGACYSPED.IEP miep on mstu.StudentRefID = miep.StudentRefID LEFT JOIN 
+--	dbo.PrgInvolvement mx on mstu.DestID = mx.StudentID and mx.ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' left join 
+--	LEGACYSPED.MAP_PrgInvolvementID mm on miep.StudentRefID = mm.StudentRefID LEFT JOIN
+--	-- identify students that already have a sped invovlement that will overlap with this involvement
+--	dbo.PrgInvolvement mt on 
+--		(mstu.DestID = mt.StudentID and 
+--		mt.ProgramID = 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C' 
+--		and
+--	--	dbo.DateRangesOverlap (miep.IEPStartDate, miep.IEPEndDate, t.StartDate, t.EndDate, null) = 1)
+--		dbo.DateInRange( miep.IEPStartDate, mt.StartDate, mt.EndDate ) = 1)
+--WHERE 
+--	miep.IEPStartDate is not null 
+--group by mstu.StudentRefID
+
+
+
+--select StudentRefID, count(*) tot
+--from LEGACYSPED.Transform_PrgInvolvement 
+--group by StudentRefID
+--having count(*) > 1
+
+
+
+-- select * from LEGACYSPED.MAP_PrgInvolvementID 
+
+
 		/*			-- TEST to see invalid current data alongside output of this transform (add columns to select list and group by)
 		and not (
 		(t.EndDate is null and t.EndStatus is null and t.IsManuallyEnded = 0) 
@@ -57,96 +120,7 @@ AS
 		(t.EndDate is not null and t.EndStatus is not null)
 		)
 		*/
-	GROUP BY stu.StudentRefID, coalesce(x.ID, t.ID, m.DestID), stu.DestID
-GO
---
 
--- select * from LEGACYSPED.MAP_PrgInvolvementID delete
-
-
-
-/*
-
-
-
--- set transaction isolation level read committed
-
-
-
-delete from LEGACYSPED.MAP_PrgInvolvementID 
-
-
-
-
-
-
-
-Import strategy:  
-	don't touch sis data (no updates or deletes)
-	delete missing map records
-		legacy student PrgInvolvement data remains in dbo.PrgInvolvement
-			If we can devise a way to programmatically set dbo.Student.IsActive = 0 before / during / after we delete them from MAP, the "orphaned" PrgInvolvement records won't be a problem. 
-			I don't like leaving these records, but not sure how to avoid it.
-	update only legacy student records
-	insert legacy student records that don't exist in dbo.Student 
-
-
-
-GEO.ShowLoadTables PrgInvolvement
-
-set nocount on;
-declare @n varchar(100) ; select @n = 'PrgInvolvement'
-declare @t uniqueidentifier ; select @t = id from VC3ETL.LoadTable where ExtractDatabase = '29D14961-928D-4BEE-9025-238496D144C6' and DestTable = @n
-update t set 
-	SourceTable = 'LEGACYSPED.Transform_PrgInvolvement'
-	, HasMapTable = 1
-	, MapTable = 'LEGACYSPED.MAP_'+@n+'ID'   -- use this update for looksups only
-	, KeyField = 'StudentRefID'
-	, DeleteKey = 'DestID'
-	, DeleteTrans = 1
-	, UpdateTrans = 1
-	, DestTableFilter = NULL
-	, Enabled = 1
-	from VC3ETL.LoadTable t where t.ID = @t
-exec VC3ETL.LoadTable_Run @t, '', 1, 0
-print '
-
-select * from '+@n
-
-
-select d.* 
--- DELETE LEGACYSPED.MAP_PrgInvolvementID
-FROM LEGACYSPED.Transform_PrgInvolvement AS s RIGHT OUTER JOIN 
-	LEGACYSPED.MAP_PrgInvolvementID as d ON s.DestID = d.DestID
-WHERE (s.DestID IS NULL)
-
-
-select d.*
--- UPDATE PrgInvolvement SET ProgramID=s.ProgramID, StudentID=s.StudentID, StartDate=s.StartDate, EndDate=s.EndDate, IsManuallyEnded=s.IsManuallyEnded, VariantID=s.VariantID
-FROM  PrgInvolvement d JOIN 
-	LEGACYSPED.Transform_PrgInvolvement  s ON s.DestID=d.ID
-
--- INSERT LEGACYSPED.MAP_PrgInvolvementID
-SELECT StudentRefID, NEWID()
-FROM LEGACYSPED.Transform_PrgInvolvement s
-WHERE NOT EXISTS (SELECT * FROM PrgInvolvement d WHERE s.DestID=d.ID)
-
--- INSERT PrgInvolvement (ID, ProgramID, StudentID, StartDate, EndDate, IsManuallyEnded, VariantID)
-SELECT s.DestID, s.ProgramID, s.StudentID, s.StartDate, s.EndDate, s.IsManuallyEnded, s.VariantID
-FROM LEGACYSPED.Transform_PrgInvolvement s
-WHERE NOT EXISTS (SELECT * FROM PrgInvolvement d WHERE s.DestID=d.ID)
-
-
-select * from PrgInvolvement
-
-
-select * from LEGACYSPED.MAP_PrgInvolvementID
-
-select count(*) from LEGACYSPED.Transform_PrgInvolvement 
-
-
-
-*/
 
 
 
