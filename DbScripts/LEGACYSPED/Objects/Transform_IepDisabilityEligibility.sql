1837d29e32c9ade14cb92afef80d0e2ef8bdfab9
George Giddens <github@georgegiddens.com>
2012-04-29 17:06:33 -0400

    Added new view StudentDisabilitySequenced in attempt to optimize query. Kept this view even though query performance may have improved as a result of indexing changes.

--- DbScripts/LEGACYSPED/Objects/Transform_IepDisabilityEligibility.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_IepDisabilityEligibility.sql
@@ -48,6 +48,17 @@ as
 go
 
 
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'LEGACYSPED.StudentDisabilitySequenced') AND OBJECTPROPERTY(id, N'IsView') = 1)
+DROP VIEW LEGACYSPED.StudentDisabilitySequenced
+GO
+
+create view LEGACYSPED.StudentDisabilitySequenced -- attempting to resolve a performance issue by sequencing the disabilities in this view instad of in the Transform.  This view seems very fast
+as
+	select StudentRefID, DisabilityCode, 
+		Sequence = (select count(*)+1 from LEGACYSPED.StudentDisabilityPivot q where p.StudentRefID = q.StudentRefID and q.DisabSeq < p.DisabSeq)
+	from LEGACYSPED.StudentDisabilityPivot p
+go
+
 
 IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'LEGACYSPED.Transform_IepDisabilityEligibility') AND OBJECTPROPERTY(id, N'IsView') = 1)
 DROP VIEW LEGACYSPED.Transform_IepDisabilityEligibility
@@ -60,20 +71,48 @@ AS
 		DestID = me.DestID,
 		InstanceID = m.DestID, 
 		DisabilityID = d.DestID, 
-		Sequence = (select count(*)+1 from LEGACYSPED.StudentDisabilityPivot where StudentRefID = s.StudentRefID and DisabSeq < s.DisabSeq),
+		s.Sequence,
 		IsEligibileID = 'B76DDCD6-B261-4D46-A98E-857B0A814A0C', -- Only Eligible disabilities provided  
-		FormInstanceID = cast(NULL as uniqueidentifier), -- select iep.ieprefid
-		PrimaryOrSecondaryID = case when s.DisabSeq = 1 then 'AF6825FF-336C-42CE-AF57-CD095CD0DD2C' else '51619296-9938-4977-8B5F-A6E0FAEE4294' end -- snatched these from Lee Template.  Consistent everywhere?
+		FormInstanceID = cast(NULL as uniqueidentifier), 
+		PrimaryOrSecondaryID = case when s.Sequence = 1 then 'AF6825FF-336C-42CE-AF57-CD095CD0DD2C' else '51619296-9938-4977-8B5F-A6E0FAEE4294' end -- snatched PrimaryOrSecondaryID values from Lee Template.  Consistent everywhere?
 	FROM
 		LEGACYSPED.Transform_PrgIep iep JOIN
 		LEGACYSPED.MAP_PrgSectionID m on 
 			iep.VersionDestID  = m.VersionID and
 			m.DefID = 'F050EF5E-3ED8-43D5-8FE7-B122502DE86A' JOIN
-		dbo.IepEligibilityDetermination ed on m.DestID = ed.ID JOIN -- Only purpose is to ensure that this record exists.  
-		LEGACYSPED.StudentDisabilityPivot s on iep.StudentRefID = s.StudentRefID JOIN
+		LEGACYSPED.StudentDisabilitySequenced s on iep.StudentRefID = s.StudentRefID JOIN
 		LEGACYSPED.Transform_IepDisability d on s.DisabilityCode = d.DisabilityCode left join
 		LEGACYSPED.MAP_IepDisabilityEligibilityID me on 
 			iep.IepRefID = me.IepRefID and
 			d.DestID = me.DisabilityID
+	WHERE m.DestID in (select ID from dbo.IepEligibilityDetermination)
 GO
 --
+/*
+-- the inline query to derive a disability sequence from  studentdisabilitypivot is apparently causing a severe performance issue
+
+select *
+from (
+	select StudentRefID, count(*) Total, min(DisabSeq) minSeq, max(DisabSeq) maxSeq
+	from LEGACYSPED.StudentDisabilityPivot p
+	group by StudentRefID
+	having count(*) > 1
+	) q
+where Total <> maxSeq
+
+
+select s.*
+from (
+	select t.StudentRefID, count(*) Total, min(t.Sequence) minSeq, max(Sequence) maxSeq
+	from (
+		select StudentRefID, DisabilityCode, 
+			Sequence = (select count(*)+1 from LEGACYSPED.StudentDisabilityPivot q where p.StudentRefID = q.StudentRefID and p.DisabSeq < q.DisabSeq)
+		from LEGACYSPED.StudentDisabilityPivot p
+		) t
+	group by t.StudentRefID
+	) s
+where Total <> maxSeq
+
+*/
+
+ 
