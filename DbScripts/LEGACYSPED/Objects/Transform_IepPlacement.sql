4be7a8d47a48295c473918c9b2accb736a53b3a3
George Giddens <github@georgegiddens.com>
2012-04-29 17:21:41 -0400

    Renamed Primary key.  This is in an IF block that is only executed once, so will only affect future initial imports. Added Index IX_LEGACYSPED_MAP_IepPlacementID_DestID. Removed Transform_PrgIEP from query to optimize. Added isnulls to join clauses to avoid handling NULL to NULL. Query performance improved dramatically, more likely due to Indexing changes.

--- DbScripts/LEGACYSPED/Objects/Transform_IepPlacement.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_IepPlacement.sql
@@ -15,18 +15,21 @@ CREATE TABLE LEGACYSPED.MAP_IepPlacementID
 (
 	IepRefID	varchar(150) NOT NULL,
 	TypeId uniqueidentifier not null,
-	DestID uniqueidentifier NOT NULL -- this is the id of the iepplacement recod
+	DestID uniqueidentifier NOT NULL 
 )
 
 ALTER TABLE LEGACYSPED.MAP_IepPlacementID ADD CONSTRAINT
-PK_MAP_IepPlacement PRIMARY KEY CLUSTERED
+PK_MAP_IepPlacementID PRIMARY KEY CLUSTERED
 (
 	IepRefID,
 	TypeId
 )
 END
-GO
 
+if not exists (select 1 from sys.indexes where name = 'IX_LEGACYSPED_MAP_IepPlacementID_DestID')
+create nonclustered index IX_LEGACYSPED_MAP_IepPlacementID_DestID on LEGACYSPED.MAP_IepPlacementID (DestID)
+
+GO
 
 IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'LEGACYSPED.Transform_IepPlacement') AND OBJECTPROPERTY(id, N'IsView') = 1)
 	DROP VIEW LEGACYSPED.Transform_IepPlacement
@@ -35,20 +38,21 @@ GO
 CREATE VIEW LEGACYSPED.Transform_IepPlacement
 AS
 	SELECT
-		iep.IepRefID,
+		lre.IepRefID,
 		DestID = m.DestID,
 		InstanceID = lre.DestID,
-		TypeID = t.ID,
-		OptionID = case when p.TypeID = t.ID then p.DestID else NULL End,
-		IsEnabled = case when p.TypeID = t.ID then 1 else 0 End,
-		IsDecOneCount = case when p.TypeID = t.ID then 1 else 0 End -- select t.ID TypeID
-	FROM dbo.IepPlacementType t CROSS JOIN
-		LEGACYSPED.Transform_PrgIep iep JOIN 
-		LEGACYSPED.Transform_IepLeastRestrictiveEnvironment lre ON iep.IepRefID = lre.IepRefId LEFT JOIN 
-		LEGACYSPED.Transform_IepPlacementOption p on 
-			iep.AgeGroup = p.PlacementTypeCode AND
-			iep.LRECode = p.PlacementOptionCode LEFT JOIN 
-		LEGACYSPED.MAP_IepPlacementID m on m.IepRefID = iep.IepRefID and t.ID = m.TypeID LEFT JOIN 
-		dbo.IepPlacement place on lre.DestID = place.ID 
+		TypeID = pt.ID,
+		OptionID = case when po.TypeID = pt.ID then po.DestID else NULL End,
+		IsEnabled = case when po.TypeID = pt.ID then 1 else 0 End,
+		IsDecOneCount = case when po.TypeID = pt.ID then 1 else 0 End
+	FROM dbo.IepPlacementType pt CROSS JOIN
+		LEGACYSPED.Transform_IepLeastRestrictiveEnvironment lre LEFT JOIN -- attempting to address a performance issue when treating nulls in queries referencing this view
+		LEGACYSPED.Transform_IepPlacementOption po on 
+			isnull(lre.AgeGroup,'a') = isnull(po.PlacementTypeCode,'b') AND
+			isnull(lre.LRECode,'a') = isnull(po.PlacementOptionCode,'b') LEFT JOIN 
+		LEGACYSPED.MAP_IepPlacementID m on 
+			isnull(m.IepRefID,'a') = isnull(lre.IepRefID,'b') and 
+			isnull(m.TypeID,'00000000-0000-0000-0000-000000000000') = isnull(pt.ID,'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF') LEFT JOIN 
+		dbo.IepPlacement t on lre.DestID = t.ID -- 1:24 for 23556 records.  Attempts to address performance issues not working well
 GO
--- 
+--
