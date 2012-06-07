b4c1cc1dc8b74fea79a8242f5ad6743546e87fd2
George Giddens <github@georgegiddens.com>
2012-06-07 13:01:32 -0400

    minor modification

--- Support/upgrade_db_VERSION.bat
+++ Support/upgrade_db_VERSION.bat
@@ -1,6 +1,6 @@
 
-set modulesdirectory="E:\Sites\Enrich\Excent Enrich DC7 FL Lee\Setup\7.25.2.3314\Product\DbScripts"
-set connection="server=.;uid=DC7_FL_Lee_User;pwd=vc3go!!;database=Enrich_DC7_FL_Lee"
+set modulesdirectory="E:\Sites\Enrich\Excent Enrich DC5 CO Poudre\Setup\7.26.4.3419\Product\DbScripts"
+set connection="server=.;uid=CO_UPB_User;pwd=vc3go!!;database=Enrich_COUPB"
 ExecuteTask\ExecuteTask.exe upgradedb modulesdirectory=%modulesdirectory% connectionstring=%connection% ignoremissingmodules=true
 
 
