8d59dc5b370190c5e018d6ce2c463b425483bbd0
George Giddens <github@georgegiddens.com>
2012-06-22 20:05:18 -0400

    Modified to make it easier to map records if some records can be deleted without mapping.  This strategy can be used similarly for other SelectLists target tables. Amended commit - improved comments.

--- DbScripts/Prepare DB - IepDisability.sql
+++ DbScripts/Prepare DB - IepDisability.sql
@@ -16,21 +16,89 @@ insert @IepDisability  (ID, Name, Definition, DeterminationFormTemplateID, State
 insert @IepDisability  (ID, Name, Definition, DeterminationFormTemplateID, StateCode) values ('E77D061F-0E5B-4CF8-AB46-A0D4042E8601', 'Vision Impairment, Including Blindness', '<b>Definition:</b> A student with vision impairment, including blindness shall have a deficiency in visual acuity and/or visual field and/or visual performance where, even with the use of lenses or corrective devices, he/she is prevented from receiving reasonable educational benefit from general education.', '71BE7387-5ED1-470C-9A79-0C0974A9627E', '06')
 
 
------- insert test
---select t.ID, t.Name, t.Definition, t.DeterminationFormTemplateID, t.StateCode
---from IepDisability x right join
---@IepDisability t on x.ID = t.ID 
---where x.ID is null
---order by x.Name
+---- insert test
+select t.ID, t.Name, t.Definition, t.DeterminationFormTemplateID, t.StateCode
+from IepDisability x right join
+@IepDisability t on x.ID = t.ID 
+where x.ID is null order by x.Name
 
------- delete test
---select x.*, t.StateCode
---from IepDisability x left join
---@IepDisability t on x.ID = t.ID 
---where t.ID is null order by x.Name
+---- delete test
+select x.*, t.StateCode
+from IepDisability x left join
+@IepDisability t on x.ID = t.ID 
+where t.ID is null order by x.Name
 
+
+declare @RelSchema varchar(100), @RelTable varchar(100), @RelColumn varchar(100), @KeepID varchar(50), @TossID varchar(50), @toss varchar(50);
 
 begin tran FixDisab
+
+
+/* ============================================================================= NOTE ============================================================================= 
+
+	This cursor is to delete as many values as possible without having to MAP them visually below in order to save time and effort that would be required to 
+	match them by sight.
+
+   ============================================================================= NOTE ============================================================================= */
+
+
+declare T cursor for 
+select x.ID
+from IepDisability x left join
+@IepDisability t on x.ID = t.ID 
+where t.ID is null 
+
+open T
+fetch T into @toss
+
+while @@fetch_status = 0
+
+begin
+
+	declare R cursor for 
+	SELECT 
+		SCHEMA_NAME(f.SCHEMA_ID) SchemaName,
+		OBJECT_NAME(f.parent_object_id) AS TableName,
+		COL_NAME(fc.parent_object_id,fc.parent_column_id) AS ColumnName
+	FROM sys.foreign_keys AS f
+		INNER JOIN sys.foreign_key_columns AS fc ON f.OBJECT_ID = fc.constraint_object_id
+		INNER JOIN sys.objects AS o ON o.OBJECT_ID = fc.referenced_object_id
+	where SCHEMA_NAME(o.SCHEMA_ID) = 'dbo' 
+		and OBJECT_NAME (f.referenced_object_id) = 'IepDisability' ------------------------- Table name here
+		and COL_NAME(fc.referenced_object_id,fc.referenced_column_id) = 'ID' --------------- Column name here
+	order by SchemaName, TableName, ColumnName
+
+	open R
+	fetch R into @relschema, @RelTable, @relcolumn
+
+	while @@fetch_status = 0
+	begin
+
+ 	exec ('delete x from dbo.IepDisability x left join '+@RelTable+' r on x.ID = r.'+@RelColumn+' where x.ID = '''+@toss+''' and r.'+@RelColumn+' is null')
+
+-- print 
+
+	fetch R into @relschema, @RelTable, @relcolumn
+	end
+	close R
+	deallocate R
+
+fetch T into @toss
+end
+close T
+deallocate T
+
+
+
+---- delete test -------------------------------------------------------- If there remain no values to be updated, the following query will return 0 rows.  
+---- In this case the section "populate MAP" can be skipped
+select x.*, t.StateCode
+from IepDisability x left join
+@IepDisability t on x.ID = t.ID 
+where t.ID is null order by x.Name
+
+
+
 -- update state code
 update x set StateCode = t.StateCode
 -- select g.*, t.StateCode
@@ -46,7 +114,20 @@ where x.ID is null
 order by x.Name
 
 
+
 declare @MAP_IepDisability table (KeepID uniqueidentifier, TossID uniqueidentifier)
+
+
+/* ============================================================================= NOTE ============================================================================= 
+
+	It is expected that the ID values in the rows below for Autism, Hearing Impairment, Preschooler with a Disability, Serious Emotional and Vision Impairment
+	will need to be updated for all hosted districts in Coloardo.  
+	
+	HOWEVER
+	
+	it is very important to verify that these are the only values need to be updated / deleted.  Please use the     "delete test"    query above to verify.
+
+   ============================================================================= NOTE ============================================================================= */
 
 
 --B9AAA2A4-A395-4BF2-B5C1-6AF98CCEA676	Autism Spectrum Disorders
@@ -55,25 +136,22 @@ declare @MAP_IepDisability table (KeepID uniqueidentifier, TossID uniqueidentifi
 --7599B90D-8842-4B49-9BFC-B5CDBAAAA074	Serious Emotional Disability
 --CF411FEB-F76E-4EC0-BE2F-0F84AA453292	Vision Impairment, Including Blindness
 
--- populate a mapping that to update FK related records of IepDisability records that will be deleted   
-	-- this needs to be done by visual inspection because IepDisability names can vary widely
--- 1. un-comment the rows required to map for updating FK related tables.  
--- 2. Add the ID that needs to be deleted in the Empty quotes of the Values insert
-insert @MAP_IepDisability  values ('BBB4773F-4A8A-49E5-A0D4-952D2A0D1F18', 'B9AAA2A4-A395-4BF2-B5C1-6AF98CCEA676') -- 'Autism Spectrum Disorders'
-insert @MAP_IepDisability  values ('0C02B702-D681-4C66-BA67-8F9D3847E6A9', 'D1CFA1E9-D8D8-4317-92A4-94621F5C3466') -- 'Hearing Impairment, Including Deafness'
+-- populate MAP
+-- this needs to be done by visual inspection because IepDisability names can vary widely
+--insert @MAP_IepDisability  values ('BBB4773F-4A8A-49E5-A0D4-952D2A0D1F18', 'B9AAA2A4-A395-4BF2-B5C1-6AF98CCEA676') -- 'Autism Spectrum Disorders'
+--insert @MAP_IepDisability  values ('0C02B702-D681-4C66-BA67-8F9D3847E6A9', 'D1CFA1E9-D8D8-4317-92A4-94621F5C3466') -- 'Hearing Impairment, Including Deafness'
 --insert @MAP_IepDisability  values ('12F08D30-1ADA-4F9A-AD2A-EF5451BB2325', '') -- 'Intellectual Disability'
 --insert @MAP_IepDisability  values ('CA41A561-16BE-4E21-BE8A-BC59ED86C921', '') -- 'Multiple Disabilities'
 --insert @MAP_IepDisability  values ('07093979-0C3F-414D-9750-8080C6BB7C45', '') -- 'Physical Disability'
-insert @MAP_IepDisability  values ('917D79C8-8604-49E5-A64F-0ADCFD85819B', '1D0B34DD-55BF-42EB-A0CA-7D2542EBC059') -- 'Preschooler with a Disability'
-insert @MAP_IepDisability  values ('41500452-3FEB-4EBF-87A0-BFC5F385A973', '7599B90D-8842-4B49-9BFC-B5CDBAAAA074') -- 'Serious Emotional Disability'
+--insert @MAP_IepDisability  values ('917D79C8-8604-49E5-A64F-0ADCFD85819B', '1D0B34DD-55BF-42EB-A0CA-7D2542EBC059') -- 'Preschooler with a Disability'
+--insert @MAP_IepDisability  values ('41500452-3FEB-4EBF-87A0-BFC5F385A973', '7599B90D-8842-4B49-9BFC-B5CDBAAAA074') -- 'Serious Emotional Disability'
 --insert @MAP_IepDisability  values ('0E026822-6B22-43A1-BD6E-C1412E3A6FA3', '') -- 'Specific Learning Disability'
 --insert @MAP_IepDisability  values ('96A66C19-7AAB-4EE6-AA81-7CD8CD4FEB09', '') -- 'Speech or Language Impairment'
 --insert @MAP_IepDisability  values ('E65E6AAE-AE9B-4275-B547-9CE1A4B9EEF3', '') -- 'Traumatic Brain Injury'
-insert @MAP_IepDisability  values ('E77D061F-0E5B-4CF8-AB46-A0D4042E8601', 'CF411FEB-F76E-4EC0-BE2F-0F84AA453292') -- 'Vision Impairment, Including Blindness'
+--insert @MAP_IepDisability  values ('E77D061F-0E5B-4CF8-AB46-A0D4042E8601', 'CF411FEB-F76E-4EC0-BE2F-0F84AA453292') -- 'Vision Impairment, Including Blindness'
 
 -- list all tables with FK on GradeLevel and update them 
 
-declare @RelSchema varchar(100), @RelTable varchar(100), @RelColumn varchar(100), @KeepID varchar(50), @TossID varchar(50)
 
 declare I cursor for 
 select KeepID, TossID from @MAP_IepDisability 
@@ -127,4 +205,19 @@ where t.ID is null
 
 commit tran FixDisab
 
+
+---- insert test (should return no rows)
+select t.ID, t.Name, t.Definition, t.DeterminationFormTemplateID, t.StateCode
+from IepDisability x right join
+@IepDisability t on x.ID = t.ID 
+where x.ID is null order by x.Name
+
+---- delete test (should return no rows)
+select x.*, t.StateCode
+from IepDisability x left join
+@IepDisability t on x.ID = t.ID 
+where t.ID is null order by x.Name
+
+-- View the final result.  Compare to the SelectLists template 
+select * from IepDisability d order by d.Name
 
