cf773ba6b56f5e7bf515e5ef7816b6005b7bbd12
George Giddens <github@georgegiddens.com>
2012-06-27 23:57:37 -0400

    Commented out test queries and changed delete query from and where clauses.

--- DbScripts/Prepare DB - IepGoalAreaDef.sql
+++ DbScripts/Prepare DB - IepGoalAreaDef.sql
@@ -2,8 +2,6 @@
 	
 	Before using this script you must update the section below to populate the @MAP_IepGoalAreaDef table
 	with the KeepID and the TossID.   
-	
-	
 
    ============================================	IMPORTANT! ============================================ */
 Begin tran fixgoal
@@ -20,38 +18,29 @@ insert @IepGoalAreaDef (ID,Sequence,Name,AllowCustomProbes ,StateCode,RequireGoa
 insert @IepGoalAreaDef (ID,Sequence,Name,AllowCustomProbes ,StateCode,RequireGoal) values ('4F131BE0-D2A9-4EB2-8639-D772E05F3D5E',4,'Time Management',0,NULL,1)
 insert @IepGoalAreaDef (ID,Sequence,Name,AllowCustomProbes ,StateCode,RequireGoal) values ('37EA0554-EC3F-4B95-AAD7-A52DECC7377C',1,'Writing',0,NULL,1)
 
-select * from IepGoalAreaDef order by Name
-select * from @IepGoalAreaDef order by Name
+--select * from IepGoalAreaDef order by Name
+--select * from @IepGoalAreaDef order by Name
 
---deleted the IepGoalAreaDef where DeletedDate is not null(Delete Unneeded)
-delete g
- --select g.*
-from IepGoalAreaDef g
----- insert test
-where g.DeletedDate is not null
+----insert test
+--select t.ID, t.Sequence , t.Name,t.StateCode,t.RequireGoal
+--from IepGoalAreaDef g right join
+--@IepGoalAreaDef t on g.ID = t.ID 
+--where g.ID is null
+--order by  g.Name
 
---insert test
-select t.ID, t.Sequence , t.Name,t.StateCode,t.RequireGoal
-from IepGoalAreaDef g right join
-@IepGoalAreaDef t on g.ID = t.ID 
-where g.ID is null
-order by  g.Name
-
----- delete test
-select g.*, t.StateCode
-from IepGoalAreaDef g left join
-@IepGoalAreaDef t on g.ID = t.ID 
-where t.ID is null
+------ delete test
+--select g.*, t.StateCode
+--from IepGoalAreaDef g left join
+--@IepGoalAreaDef t on g.ID = t.ID 
+--where t.ID is null
 
 
--- update state code
-update g set StateCode = t.StateCode,
-		 Name = t.Name,
-		 RequireGoal = t.RequireGoal
+update t set RequireGoal = g.RequireGoal,
+		 Sequence = g.Sequence,
+		 AllowCustomProbes = g.AllowCustomProbes
 --select g.*, t.StateCode
 from IepGoalAreaDef g  join
-@IepGoalAreaDef t on g.ID = t.ID 
-where g.Sequence not in (99)
+@IepGoalAreaDef t on g.Name = t.Name 
 
 -- insert missing.  This has to be done before updating the records to be deleted and before deleting.
 insert IepGoalAreaDef (ID,Sequence,Name,StateCode,AllowCustomProbes,RequireGoal)
@@ -98,7 +87,7 @@ begin
 		INNER JOIN sys.foreign_key_columns AS fc ON f.OBJECT_ID = fc.constraint_object_id
 		INNER JOIN sys.objects AS o ON o.OBJECT_ID = fc.referenced_object_id
 	where SCHEMA_NAME(o.SCHEMA_ID) = 'dbo' 
-		and OBJECT_NAME (f.referenced_object_id) = 'IepGoalAreaDef' 
+		and OBJECT_NAME (f.referenced_object_id) = 'IepGoalAreaDef' ------------------------------- table name
 		and COL_NAME(fc.referenced_object_id,fc.referenced_column_id) = 'ID'
 	order by SchemaName, TableName, ColumnName
 
@@ -122,23 +111,11 @@ deallocate I
 
 delete g
  --select g.*, t.StateCode
-from IepGoalAreaDef g left join
-@IepGoalAreaDef t on g.ID = t.ID 
-where t.ID is null 
+from IepGoalAreaDef g join
+@MAP_IepGoalAreaDef t on g.ID = t.TossID 
+
 
 commit tran fixgoal
 
 --ROLLBACK  tran fixgoal
-
-
---select * from IepGoalAreaDef  order by Name 
---51C976DF-DC56-4F89-BCA1-E9AB6A01FBE7			Communication
---6BBAADD8-BD9D-4C8F-A573-80F136B0A9FB			Emotions
---0E95D360-5CBE-4ECA-820F-CC25864D70D8			Mathematics
---0C0783DD-3D11-47A2-A1C1-CFE2F8F1FB4C			Organization
---702A94A6-9D11-408B-B003-11B9CCDE092E			Other
---504CE0ED-537F-4EA0-BD97-0349FB1A4CA8			Reading
---25D890C3-BCAE-4039-AC9D-2AE21686DEB0			Self Advocacy
---4F131BE0-D2A9-4EB2-8639-D772E05F3D5E			Time Management
---37EA0554-EC3F-4B95-AAD7-A52DECC7377C			Writing
 
