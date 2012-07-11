4fcaca5f2de2a8a8124ed7812d712bbf90b90747
Muthu <muthuveerappan0104@gmail.com>
2012-07-10 22:38:16 -0700

    Modified the view by adding Map_table in Join and change the selection condition for StateCode.

--- DbScripts/LEGACYSPED/Objects/Transform_Ethnicity.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_Ethnicity.sql
@@ -23,16 +23,29 @@ GO
 -- should we update the statecode column of the enum value table and then join statecode to statecode?  in this case EnumValue.Code = LEGACYSPED.SelectLists.StateCode so we're doing the Q&D
 create view LEGACYSPED.Transform_Ethnicity
 as
+
+-- join to EnumValue on MAP, EnrichID, StateCode
 SELECT
-	LegacySpedCode = ISNULL(l.LegacySpedCode,x.Code),
-	StateCode = ISNUll( x.StateCode, l.StateCode),
-	DestID = coalesce(x.ID, l.EnrichID, NEWID()),
-	Name = l.EnrichLabel
+	LegacySpedCode = coalesce(x.Code, isnull(l.LegacySpedCode, convert(varchar(150), l.EnrichLabel))),
+	StateCode = coalesce( s.StateCode, x.StateCode, l.StateCode),
+	DestID = coalesce(s.ID, x.ID, l.EnrichID, m.DestID),
+	Name = coalesce(s.DisplayValue, x.DisplayValue, l.EnrichLabel)
 FROM
 	LEGACYSPED.SelectLists l LEFT JOIN
-	EnumValue x on l.LegacySpedCode = x.Code -- this will not work for all states.  need a different approach.
+	dbo.EnumValue s on l.StateCode = s.StateCode left join
+	LEGACYSPED.MAP_EthnicityID m on l.LegacySpedCode = m.EthnicityCode and l.Type = 'Ethnic' left join
+	dbo.EnumValue x on l.LegacySpedCode = x.Code -- this will not work for all states.  need a different approach.
 WHERE l.Type = 'Ethnic' AND
 	x.Type = (select t.ID from EnumType t where t.Type = 'ETH')
 GO
 
 --
+--select * from EnumType where Type = 'ETH'
+--select * from EnumValue v where v.Type = 'CBB84AE3-A547-4E81-82D2-060AA3A50535'
+
+--select * from VC3ETL.LoadTable where ExtractDatabase = '29D14961-928D-4BEE-9025-238496D144C6' order by Sequence
+
+
+
+
+
