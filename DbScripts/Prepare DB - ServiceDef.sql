4876d1662aaa196d136ad076afebd40f9a32a2a0
Muthu <muthuveerappan0104@gmail.com>
2012-07-17 09:36:53 -0700

    Modified the script by adding the cateogryID in Iepservice insert statement.

--- DbScripts/Prepare DB - ServiceDef.sql
+++ DbScripts/Prepare DB - ServiceDef.sql
@@ -181,15 +181,11 @@ delete x
 from ServiceDef x join
 @MAP_ServiceDef t on x.ID = t.TossID 
 
-insert IepServiceDef (ID, ScheduleFreqOnly) 
-select s.ID, 0
-from @ServiceDef s left join
-IepServiceDef t on s.ID = t.ID
-where t.ID is null
 
 
-insert IepServiceDef (ID, ScheduleFreqOnly) 
-select s.ID, 0
+
+insert IepServiceDef (ID, CategoryID, ScheduleFreqOnly) 
+select s.ID, s.CategoryID,  0
 from @ServiceDef s left join
 IepServiceDef t on s.ID = t.ID
 where t.ID is null
