9dd88ae1fb5ca1695f10907664efe0b4dd7f821e
Muthu <muthuveerappan0104@gmail.com>
2012-09-04 02:05:45 -0700

    Committed.

--- Support/upgrade_db_ST_District.bat
+++ Support/upgrade_db_ST_District.bat
@@ -1,9 +1,9 @@
-set state=CO
+set state=IL
 
-set speddistrict=Mesa51
+set speddistrict=Waukegan
 
 echo DB Upgrade %state% %speddistrict%
 
-set connection=server=10.0.1.30;uid=DCB2_CO_Mesa51;pwd=vc3go!!;database=Enrich_DCB2_CO_Mesa51
+set connection=server=10.0.1.30;uid=DCB4;pwd=vc3go!!;database=Enrich_DCB4
 
 ExecuteTask\ExecuteTask.exe project projectfile="upgrade_db_project.xml" $connection="%connection%" $state="%state%" $speddistrict="%speddistrict%"
