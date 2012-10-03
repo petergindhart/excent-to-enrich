53b94793db35dbc245e7067f245387fe70678403
George Giddens <github@georgegiddens.com>
2012-10-03 16:24:49 -0400

    using batch file variables now.  corrected error with exe path.

--- Support/service_uninstall.bat
+++ Support/service_uninstall.bat
@@ -1,1 +1,5 @@
-ExecuteTask\ExecuteTask.exe uninstallservice ServiceExe="E:\Inetpub\Sites\EnrichCOUPB\TasksService" ServiceName="VC3 TestView Scheduled Tasks"
+
+set newsvc="VC3 Scheduled Tasks (DC5)"
+set exe="E:\Sites\Enrich\Excent Enrich DC5 CO Poudre\TasksService\TestViewTasksService.exe"
+
+ExecuteTask\ExecuteTask.exe uninstallservice ServiceExe=%exe% ServiceName=%newsvc%
