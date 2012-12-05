0c92a3f0fa0b578e93e38781b47bf4550030a915
Muthu <muthuveerappan0104@gmail.com>
2012-12-05 01:15:04 -0700

    Added the where clause  iep.DonotTouch = 0in code

--- DbScripts/LEGACYSPED/Objects/Transform_IepServices.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_IepServices.sql
@@ -133,6 +133,7 @@ AS
 		LEGACYSPED.MAP_FormInstance_Services mfi on iep.IepRefID = mfi.IEPRefID left join 
 		LEGACYSPED.MAP_FormInstanceInterval_Services mfii on iep.IepRefID = mfii.IepRefID left join 
 		LEGACYSPED.MAP_FormInputValue_Services mfiv on iep.IepRefID = mfiv.IepRefID 
+	WHERE iep.DoNotTouch = 0
 GO
 --
 
