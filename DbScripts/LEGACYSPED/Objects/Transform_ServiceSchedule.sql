b5170792661046761acc1a0b76dfee682f7d2934
George Giddens <github@georgegiddens.com>
2012-12-05 19:47:29 -0500

    use the function instead of the view

--- DbScripts/LEGACYSPED/Objects/Transform_ServiceSchedule.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_ServiceSchedule.sql
@@ -3,7 +3,7 @@ DROP VIEW LEGACYSPED.Transform_ServiceSchedule
 GO  
   
 CREATE VIEW LEGACYSPED.Transform_ServiceSchedule  
-AS  
+AS
  SELECT
   sched.DestID,
   ScheduleID = sched.DestID, 
@@ -14,11 +14,10 @@ AS
   LocationDescription = loc.Name
  FROM
   LEGACYSPED.MAP_ServicePlanID v JOIN
-  LEGACYSPED.Transform_Schedule sched on v.ServiceRefID = sched.ServiceRefID left join 
+  LEGACYSPED.Transform_Schedule_Function() sched on v.ServiceRefID = sched.ServiceRefID left join 
   LEGACYSPED.MAP_ScheduleID ssm on v.ServiceRefID = ssm.ServiceRefID LEFT JOIN
   Legacysped.Service ser ON ser.ServiceRefId = v.ServiceRefID left join 
   LEGACYSPED.Transform_PrgLocation loc on ser.ServiceLocationCode = loc.ServiceLocationCode LEFT JOIN 
   LEGACYSPED.MAP_SpedStaffMemberView prv on isnull(ser.StaffEmail,'') = prv.StaffEmail
 GO
-
 
