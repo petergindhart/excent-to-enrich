8b7759b265ff1b2f76c03bb47a972d4a356b8ca1
George Giddens <github@georgegiddens.com>
2012-12-19 01:09:05 -0500

    Function was not using the temporary tables properly.  It is now.

--- DbScripts/LEGACYSPED/Objects/Transform_IepPlacement_Function.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_IepPlacement_Function.sql
@@ -17,6 +17,24 @@ CREATE FUNCTION LEGACYSPED.Transform_IepPlacement_Function ()
 	) AS  
 	BEGIN 
 
+---------------------------------------------- test
+
+--declare @p table (
+--		IepRefID varchar(150), 
+--		DestID uniqueidentifier,
+--		InstanceID uniqueidentifier,
+--		TypeID uniqueidentifier,
+--		OptionID uniqueidentifier,
+--		SourceID uniqueidentifier,
+--		AsOfDate datetime,
+--		IsDecOneCount bit,
+--		MinutesInstruction int,
+--		UseLimitedValidation bit,
+--		DoNotTouch bit
+--	)
+---------------------------------------------- test
+
+
 declare @Transform_IepLeastRestrictiveEnvironment  table (
 IepRefId	varchar(150),
 AgeGroup	varchar(3),
@@ -80,8 +98,33 @@ insert @Transform_PrgIep
 select * from LEGACYSPED.Transform_PrgIep
 
 insert @p
-select * from LEGACYSPED.Transform_IepPlacement
-
+-- select * from LEGACYSPED.Transform_IepPlacement
+	SELECT 
+		lre.IepRefID, 
+		DestID = m.DestID,
+		InstanceID = lre.DestID,
+		TypeID = pt.ID,
+		OptionID = case when po.TypeID = pt.ID then po.DestID else NULL End,
+		SourceID = 'C9967875-E09B-4F3E-BE00-5BF2A97C7DAD', -- User Created:  Per Pete, use this in all cases as of 10/30/2012
+		AsOfDate = CASE
+			WHEN piep.StartDate >= DATEADD(YEAR, pt.MinAge, stu.DOB) THEN piep.StartDate
+			ELSE DATEADD(YEAR, pt.MinAge, stu.DOB)
+			END,
+		IsDecOneCount = case when po.TypeID = pt.ID then 1 else 0 End,
+		MinutesInstruction = lre.MinutesInstruction,
+		UseLimitedValidation = cast (1 as bit),
+		lre.DoNotTouch
+	FROM @Transform_IepLeastRestrictiveEnvironment lre JOIN -- left join resulted in more rows returned.  did not investigate.  gg
+		LEGACYSPED.Transform_IepPlacementOption po on 
+			isnull(lre.AgeGroup,'a') = isnull(po.PlacementTypeCode,'b') AND
+			isnull(lre.LRECode,'a') = isnull(po.PlacementOptionCode,'b') LEFT JOIN 
+		LEGACYSPED.MAP_IepPlacementID m on 
+			isnull(m.IepRefID,'a') = isnull(lre.IepRefID,'b') and 
+			isnull(m.TypeID,'00000000-0000-0000-0000-000000000000') = isnull(po.TypeID,'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF') LEFT JOIN 
+		dbo.IepPlacementType pt on po.TypeID = pt.ID left join
+		dbo.IepPlacement t on isnull(lre.DestID,'00000000-0000-0000-0000-000000000000') = isnull(t.InstanceID,'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF')  LEFT JOIN  -- 1:24 for 23556 records.  Attempts to address performance issues not working well
+		@Transform_PrgIep piep ON isnull(piep.IEPRefID,'a') =isnull(m.IepRefID,'b') LEFT JOIN  --For new LRE model
+		dbo.Student stu ON stu.ID = isnull(piep.StudentID,'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF')
 	return 
 	END
 GO
