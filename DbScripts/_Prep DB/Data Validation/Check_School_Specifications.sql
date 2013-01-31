f2a341b3073c84e4b3e6e6c5ef5d2b95d1193ff7
Muthu <muthuveerappan0104@gmail.com>
2013-01-31 05:28:48 -0700

    Modified

--- DbScripts/_Prep DB/Data Validation/Check_School_Specifications.sql
+++ DbScripts/_Prep DB/Data Validation/Check_School_Specifications.sql
@@ -13,21 +13,24 @@ SET @sql = 'DELETE School'
 EXEC sp_executesql @stmt = @sql
 
 INSERT School 
-SELECT CONVERT(VARCHAR(10) ,SchoolCode)
-	  ,CONVERT(VARCHAR(255),SchoolName)
-	  ,CONVERT(VARCHAR(10) ,DistrictCode)
-	  ,CONVERT(int,MinutesPerWeek)
-	FROM School_LOCAL
-    WHERE ((DATALENGTH(SchoolCode)/2)<= 10) 
-		  AND ((DATALENGTH(SchoolName)/2)<=254) 
-		  AND ((DATALENGTH(DistrictCode)/2)<= 10) 
-		  AND (ISNUMERIC(MinutesPerWeek)=1) AND ((DATALENGTH(MinutesPerWeek)/2)<= 4)  ---!!!Need to check this out!!!
-		  AND NOT EXISTS (SELECT SchoolCode,DISTRICTCODE FROM School_LOCAL GROUP BY SchoolCode,DISTRICTCODE HAVING COUNT(*)>1) ---!!!Need to check this out!!!
-		  AND (SchoolCode IS NOT NULL) 
-		  AND (SchoolName IS NOT NULL)
-		  AND (DistrictCode IS NOT NULL)
-		  AND (MinutesPerWeek IS NOT NULL)
-		  AND DistrictCode IN (SELECT DISTINCT DistrictCode FROM District_LOCAL)
+SELECT CONVERT(VARCHAR(10) ,sch.SCHOOLCODE)
+	  ,CONVERT(VARCHAR(255),sch.SCHOOLNAME)
+	  ,CONVERT(VARCHAR(10) ,sch.DISTRICTCODE)
+	  ,CONVERT(int ,sch.MINUTESPERWEEK)
+	FROM School_LOCAL sch
+		 JOIN District dt ON dt.DistrictCode = Sch.DISTRICTCODE
+		 JOIN (SELECT SchoolCode,DISTRICTCODE FROM School_LOCAL GROUP BY SchoolCode,DISTRICTCODE HAVING COUNT(*)=1) ucsch
+		 ON sch.SCHOOLCODE = ucsch.SCHOOLCODE
+    WHERE ((DATALENGTH(sch.SCHOOLCODE)/2)<= 10) 
+		  AND ((DATALENGTH(sch.SCHOOLNAME)/2)<=254) 
+		  AND ((DATALENGTH(sch.DISTRICTCODE)/2)<= 10) 
+		  AND (ISNUMERIC(sch.MINUTESPERWEEK)=1) AND ((DATALENGTH(sch.MINUTESPERWEEK)/2)<= 4)  ---!!!Need to check this out!!!
+		 -- AND NOT EXISTS (SELECT SchoolCode,DISTRICTCODE FROM School_LOCAL GROUP BY SchoolCode,DISTRICTCODE HAVING COUNT(*)>1) ---!!!Need to check this out!!!
+		  AND (sch.SCHOOLCODE IS NOT NULL) 
+		  AND (sch.SCHOOLNAME IS NOT NULL)
+		  AND (sch.DISTRICTCODE IS NOT NULL)
+		  AND (sch.MINUTESPERWEEK IS NOT NULL)
+		 -- AND DistrictCode IN (SELECT DISTINCT DistrictCode FROM District_LOCAL)
 		  
 SET @sql = 'IF OBJECT_ID(''tempdb..#School'') IS NOT NULL DROP TABLE #School'	
 EXEC sp_executesql @stmt = @sql
@@ -78,16 +81,18 @@ WHERE (MinutesPerWeek IS NULL)
 
 --To Check Duplicate Records
 INSERT District_ValidationReport (Result)
-SELECT 'The "SchoolCode" ' +SchoolCode+' is duplicated. The line no is '+CAST(LINE AS VARCHAR(50))+ '.'+ISNULL(SchoolCode,'')+'|'+ISNULL(SchoolName,'')+'|'+ISNULL(DistrictCode,'')+'|'+ISNULL(MinutesPerWeek,'')
-FROM #School 
-WHERE EXISTS (SELECT SchoolCode,DISTRICTCODE FROM School_LOCAL GROUP BY SchoolCode,DISTRICTCODE HAVING COUNT(*)>1) 
-	 
+SELECT Distinct 'The "SchoolCode" ' +SchoolCode+' is duplicated. The line no is '+CAST(LINE AS VARCHAR(50))+ '.'+ISNULL(SchoolCode,'')+'|'+ISNULL(SchoolName,'')+'|'+ISNULL(DistrictCode,'')+'|'+ISNULL(MinutesPerWeek,'')
+FROM #School tsch
+JOIN (SELECT SchoolCode,DISTRICTCODE FROM School_LOCAL GROUP BY SchoolCode,DISTRICTCODE HAVING COUNT(*)>1) ucsch
+		 ON tsch.SCHOOLCODE = ucsch.SCHOOLCODE
+ 
+	
 --To Check the Referential Integrity
-
 INSERT District_ValidationReport (Result)
-SELECT 'The DistrictCode "' +DistrictCode+'" does not exist in District File or were not validated successfully. The line no is '+CAST(LINE AS VARCHAR(50))+ '.'+ISNULL(SchoolCode,'')+'|'+ISNULL(SchoolName,'')+'|'+ISNULL(DistrictCode,'')+'|'+ISNULL(MinutesPerWeek,'')
-FROM #School
-WHERE DistrictCode IN (SELECT DISTINCT DistrictCode FROM District_LOCAL)
+SELECT 'The DistrictCode "' +tsch.DISTRICTCODE+'" does not exist in District File or were not validated successfully. The line no is '+CAST(tsch.LINE AS VARCHAR(50))+ '.'+ISNULL(tsch.SCHOOLCODE,'')+'|'+ISNULL(tsch.SCHOOLNAME,'')+'|'+ISNULL(tsch.DISTRICTCODE,'')+'|'+ISNULL(tsch.MINUTESPERWEEK,'')
+FROM #School tsch
+LEFT JOIN District dt ON dt.DistrictCode = tsch.DISTRICTCODE
+WHERE dt.DistrictCode IS NOT NULL
 
 SET @sql = 'IF OBJECT_ID(''tempdb..#School'') IS NOT NULL DROP TABLE #School'	
 EXEC sp_executesql @stmt = @sql   
