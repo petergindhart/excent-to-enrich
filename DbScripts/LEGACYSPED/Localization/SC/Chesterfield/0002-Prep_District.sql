2756446a12702ab17a34dd7ed1c09c59ab311cd1
George Giddens <github@georgegiddens.com>
2013-03-15 03:43:54 -0400

    Added new table MAP_PrgStatus_ConvertedDataPlan

--- DbScripts/LEGACYSPED/Localization/SC/Chesterfield/0002-Prep_District.sql
+++ DbScripts/LEGACYSPED/Localization/SC/Chesterfield/0002-Prep_District.sql
@@ -27,6 +27,16 @@ from @OrgUnit t join
 OrgUnit ou on t.ID = ou.ID 
 
 
+if exists (select 1 from sys.schemas s join sys.objects o on s.schema_id = o.schema_id where s.name = 'LEGACYSPED' and o.name = 'MAP_PrgStatus_ConvertedDataPlan')
+drop table LEGACYSPED.MAP_PrgStatus_ConvertedDataPlan
+go
+
+create table LEGACYSPED.MAP_PrgStatus_ConvertedDataPlan (DestID uniqueidentifier not null)
+insert LEGACYSPED.MAP_PrgStatus_ConvertedDataPlan values ('0B5D5C72-5058-4BF5-A414-BDB27BD5DD94')
+go
+
+
+
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'LEGACYSPED.ImportPrgSections') AND type in (N'U'))
 BEGIN
 CREATE TABLE LEGACYSPED.ImportPrgSections (
