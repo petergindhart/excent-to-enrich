9e920cf74bfab95d92e3ed5865febcc16dbb08f0
George Giddens <github@georgegiddens.com>
2013-05-10 13:01:34 -0400

    exec  dbo.PrgGoalProgressPeriod_EnsureForActiveGoals - note:  the GUID param has not been added yet.

--- DbScripts/LEGACYSPED/Localization/MI/Jackson/0002-Prep_District.sql
+++ DbScripts/LEGACYSPED/Localization/MI/Jackson/0002-Prep_District.sql
@@ -173,6 +173,8 @@ update d set IsReevaluationNeeded = 1, StartDate = dateadd(dd, -d.MaxDaysToCompl
 
 exec dbo.Util_VerifyProgramDataAssumptions 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C'
 
+exec dbo.PrgGoalProgressPeriod_EnsureForActiveGoals '' --- progress period GUID
+
 GO
 
 
