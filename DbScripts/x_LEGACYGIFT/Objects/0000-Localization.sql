218b37ee789fa409787f5aec98cbbe2c74320791
George Giddens <github@georgegiddens.com>
2013-06-18 13:17:56 -0400

    new

---
+++ DbScripts/x_LEGACYGIFT/Objects/0000-Localization.sql
@@ -0,0 +1,26 @@
+--#include {SpedStateInclude}\0010-Extract-Goal.sql
+--#include {SpedStateInclude}\GoalAreaExists.sql
+--#include {SpedStateInclude}\0001-Prep_State.sql
+--#include {SpedStateInclude}\Transform_IepGoalAreaDef.sql
+--#include {SpedDistrictInclude}\0002-Prep_District.sql
+--#include {SpedStateInclude}\StudentView.sql
+--#include Transform_Student.sql
+----#include Populate_MAP_StudentRefIDAll.sql
+--#include Transform_PrgInvolvement.sql
+--#include Transform_PrgItem.sql
+--#include Transform_IepServices.sql
+--#include Transform_PrgSection.sql
+--#include Transform_PrgGoals.sql
+--#include Transform_PrgGoal.sql
+--#include {SpedStateInclude}\Transform_IepGoalArea.sql
+--#include {SpedStateInclude}\Transform_IepGoalPostSchoolAreaDef.sql
+--#include {SpedStateInclude}\Transform_IepGoalSubGoalAreaDef.sql
+--#include Transform_IepPlacementOption.sql
+----#include Transform_IepServiceCategory.sql
+----#include Transform_ServiceDef.sql
+----#include Transform_ServiceProviderTitle.sql
+--#include Transform_Schedule.sql
+--#include Transform_IepService.sql
+
+-- let's reset the counts for imported data so we know this script ran when we run dc from the ui
+update et set LastSuccessfulCount = 0, CurrentCount = 0 from VC3ETL.ExtractTable et where ExtractDatabase = '29D14961-928D-4BEE-9025-238496D144C6' 
