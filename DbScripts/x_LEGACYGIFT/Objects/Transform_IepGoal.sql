d65f1506d8b444fcb23500900668b3a90493a0d8
George Giddens <github@georgegiddens.com>
2013-06-18 10:10:33 -0400

    new

---
+++ DbScripts/x_LEGACYGIFT/Objects/Transform_IepGoal.sql
@@ -0,0 +1,22 @@
+----#include Transform_PrgGoal.sql
+----#include Transform_IepGoalArea.sql
+----#include Transform_IepGoalPostSchoolArea.sql
+
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'x_LEGACYGIFT.Transform_IepGoal') AND OBJECTPROPERTY(id, N'IsView') = 1)
+DROP VIEW x_LEGACYGIFT.Transform_IepGoal
+go
+
+create view x_LEGACYGIFT.Transform_IepGoal
+as
+select 
+  ga.GoalRefID,
+  DestID = ga.GoalID,
+  GoalAreaID = ga.DestID,
+  PostSchoolAreaDefID = psa.PostSchoolAreaDefID, 
+  ga.EsyID 
+FROM x_LEGACYGIFT.Transform_IepGoalArea ga LEFT JOIN 
+	x_LEGACYGIFT.Transform_IepGoalPostSchoolAreaDef psa on ga.GoalRefID = psa.GoalRefID and psa.Sequence = 0 LEFT JOIN ---------- we cheated!
+	dbo.IepGoalArea tgt on ga.InstanceID = tgt.InstanceID and psa.PostSchoolAreaDefID = tgt.DefID 
+go
+
+
