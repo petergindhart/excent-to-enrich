e3122e94a95dab7a63c59b33203d91a6057e8627
George Giddens <github@georgegiddens.com>
2013-06-24 00:38:22 -0400

    Legacy document PDF was exported indiscriminately, but we are only importing docs for students with a PrgItem.  Ensure we import docs forst students with IEP, Gifted EP or 504 placeholder.

--- DbScripts/x_LEGACYDOC/Objects/Transform_FileData.sql
+++ DbScripts/x_LEGACYDOC/Objects/Transform_FileData.sql
@@ -27,40 +27,78 @@ GO
 
 CREATE VIEW x_LEGACYDOC.Transform_FileData
 AS
-
 /*
-	This view will select PDF document from x_LEGACYDOC.IEPDoc table by joining LEGACYSPED.IEP table on IepRefID.
+
+	Students may be involved in more than one program.  Currently there is no distinction between documents for one program or another (though IEPs are obviously sped).
+	All documents of certain types are exported from source database, regardless of the program the document was created for.  Here we ensure that if a student is involved
+	in 504, Gifted or Sped, we attach documents for them.
+
 */
-SELECT 
-	d.DocumentRefID,
-	d.DocumentType,
-	d.StudentRefID,
-	DestID = coalesce(t.ID, m.DestID),
-	OriginalName = d.DocumentType+' - '+isnull(convert(varchar, d.DocumentDate,101), ''),
-	ReceivedDate = GETDATE(),
-	d.MimeType,
-	d.Content,
-	isTemporary = 0
-FROM 
-	x_LEGACYDOC.AllDocs d JOIN
-	LEGACYSPED.IEP s ON d.StudentRefID = s.StudentRefID LEFT JOIN 
-	x_LEGACYDOC.MAP_FileDataID m ON d.DocumentRefID = m.DocumentRefID and d.DocumentType = m.DocumentType left join
-	dbo.FileData t ON m.DestID = t.ID
-union all
-SELECT 
-	d.DocumentRefID,
-	d.DocumentType,
-	d.StudentRefID,
-	DestID = coalesce(t.ID, m.DestID),
-	OriginalName = d.DocumentType+' - '+isnull(convert(varchar, d.DocumentDate,101), ''),
-	ReceivedDate = GETDATE(),
-	d.MimeType,
-	d.Content,
-	isTemporary = 0 
-FROM 
-	x_LEGACYDOC.AllDocs d JOIN 
-	x_LEGACY504.Student504Dates s on d.StudentLocalID = s.StudentNumber JOIN 
-	dbo.Student stu on s.StudentNumber = stu.Number JOIN
-	x_LEGACYDOC.MAP_FileDataID m ON d.DocumentRefID = m.DocumentRefID and d.DocumentType = m.DocumentType left join
-	dbo.FileData t ON m.DestID = t.ID
+select x.*, a.Content
+from (
+	-- SPED
+	SELECT 
+		d.DocumentRefID,
+		d.DocumentType,
+		d.StudentRefID,
+		d.StudentLocalID,
+		DestID = coalesce(t.ID, m.DestID),
+		OriginalName = d.DocumentType+' - '+isnull(convert(varchar, d.DocumentDate,101), ''),
+		ReceivedDate = GETDATE(),
+		d.MimeType,
+	-- 	d.Content,
+		isTemporary = 0
+	FROM 
+		x_LEGACYDOC.AllDocs d JOIN
+		LEGACYSPED.IEP s ON d.StudentRefID = s.StudentRefID LEFT JOIN 
+		x_LEGACYDOC.MAP_FileDataID m ON d.DocumentRefID = m.DocumentRefID and d.DocumentType = m.DocumentType left join
+		dbo.FileData t ON m.DestID = t.ID
+	union 
+	-- 504
+	SELECT 
+		d.DocumentRefID,
+		d.DocumentType,
+		d.StudentRefID,
+		d.StudentLocalID,
+		DestID = coalesce(t.ID, m.DestID),
+		OriginalName = d.DocumentType+' - '+isnull(convert(varchar, d.DocumentDate,101), ''),
+		ReceivedDate = GETDATE(),
+		d.MimeType,
+	--	d.Content,
+		isTemporary = 0 
+	FROM 
+		x_LEGACYDOC.AllDocs d JOIN 
+		x_LEGACY504.Student504Dates s on d.StudentLocalID = s.StudentNumber JOIN 
+		dbo.Student stu on s.StudentNumber = stu.Number JOIN
+		x_LEGACYDOC.MAP_FileDataID m ON d.DocumentRefID = m.DocumentRefID and d.DocumentType = m.DocumentType left join
+		dbo.FileData t ON m.DestID = t.ID
+	union 
+	-- GIFTED
+	SELECT 
+		d.DocumentRefID,
+		d.DocumentType,
+		d.StudentRefID,
+		d.StudentLocalID,
+		DestID = coalesce(t.ID, m.DestID),
+		OriginalName = d.DocumentType+' - '+isnull(convert(varchar, d.DocumentDate,101), ''),
+		ReceivedDate = GETDATE(),
+		d.MimeType,
+	-- 	d.Content,
+		isTemporary = 0
+	FROM 
+		x_LEGACYDOC.AllDocs d JOIN
+		x_LEGACYGIFT.GiftedStudent s ON d.StudentRefID = s.StudentRefID LEFT JOIN 
+		x_LEGACYDOC.MAP_FileDataID m ON d.DocumentRefID = m.DocumentRefID and d.DocumentType = m.DocumentType left join
+		dbo.FileData t ON m.DestID = t.ID
+) x join 
+x_LEGACYDOC.AllDocs a on 
+	x.DocumentRefID = a.DocumentRefID and
+	x.DocumentType = a.DocumentType and
+	x.OriginalName = a.DocumentType+' - '+isnull(convert(varchar, a.DocumentDate,101), '') 
 go
+
+
+
+
+
+
