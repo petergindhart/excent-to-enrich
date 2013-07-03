824adb4a446eb1d85f9fb082be8720327dca6ee0
George Giddens <github@georgegiddens.com>
2013-07-03 02:45:34 -0400

    Added MAP_PrgSectionID_NonVersioned

--- DbScripts/x_LEGACYGIFT/Objects/Transform_IepServices.sql
+++ DbScripts/x_LEGACYGIFT/Objects/Transform_IepServices.sql
@@ -17,6 +17,24 @@ PK_MAP_PrgSectionID PRIMARY KEY CLUSTERED
 )
 END
 
+
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'x_LEGACYGIFT.MAP_PrgSectionID_NonVersioned') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE x_LEGACYGIFT.MAP_PrgSectionID_NonVersioned
+(
+	DefID uniqueidentifier NOT NULL,
+	ItemID uniqueidentifier NOT NULL,
+	DestID uniqueidentifier NOT NULL
+)
+
+ALTER TABLE x_LEGACYGIFT.MAP_PrgSectionID_NonVersioned ADD CONSTRAINT
+PK_MAP_PrgSectionID_NonVersioned PRIMARY KEY CLUSTERED
+(
+	DefID, ItemID
+)
+END
+
+
 if not exists (select 1 from sys.indexes where name = 'IX_x_LEGACYGIFT_MAP_PrgSectionID_DestID')
 CREATE NONCLUSTERED INDEX IX_x_LEGACYGIFT_MAP_PrgSectionID_DestID ON [x_LEGACYGIFT].[MAP_PrgSectionID] ([DestID])
 
@@ -49,9 +67,6 @@ ALTER TABLE x_LEGACYGIFT.MAP_FormInputValue_Services ADD CONSTRAINT
 END
 GO
 
-
-
-
 -- #############################################################################
 IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'x_LEGACYGIFT.Transform_IepServices') AND OBJECTPROPERTY(id, N'IsView') = 1)
 DROP VIEW x_LEGACYGIFT.Transform_IepServices
@@ -64,11 +79,11 @@ AS
 		m.DestID,
 		DeliveryStatement = cast(iep.ServiceDeliveryStatement as varchar(max)), -- since Transform_IepServices is use in a lot of operations, leave the text field out of the transform for speed
 		ItemID = iep.DestID,
-	-- FormInputValue -- select * from FormInputValue
-		FormInputValue = mfiv.DestID,
+	-- FormInputValue 
+--		FormInputValue = mfiv.DestID,
 		-- IntervalID = FormInstanceIntervalID (above)
-		InputFieldId = '49DCF86E-FE6C-47DC-A8FD-D561FA564148', -- FormTemplateInputItem record for Service Delivery Statement  ----------------------- UNIQUE? NO!!!!!!!!!!!!!!
-		Sequence = 0, -- could default in VC3ETL.LoadColumn, but.....
+--		InputFieldId = '49DCF86E-FE6C-47DC-A8FD-D561FA564148', -- FormTemplateInputItem record for Service Delivery Statement  ----------------------- UNIQUE? NO!!!!!!!!!!!!!!
+--		Sequence = 0, -- could default in VC3ETL.LoadColumn, but.....
 	-- FormInputTextValue
 		--FormInputTextValueID = FormInputValue, -- (above)
 		Value = iep.ServiceDeliveryStatement -- selecting this twice on this view.  Target table column name is Value.  Source table column name is ServiceDeliveryStatement (VC3ETL.LoadColumn)
@@ -76,10 +91,10 @@ AS
 		x_LEGACYGIFT.Transform_PrgItem iep JOIN 
 		x_LEGACYGIFT.MAP_PrgSectionID_NonVersioned m on 
 			m.DefID = '8EFD24A0-46F0-4734-999A-0B4CCE2C1519' and -- gifted services
-			m.ItemID = iep.DestID left join 
-		x_LEGACYGIFT.MAP_FormInstance_Services mfi on iep.EPRefID = mfi.EPRefID left join 
-		x_LEGACYGIFT.MAP_FormInstanceInterval_Services mfii on iep.EPRefID = mfii.EPRefID left join 
-		x_LEGACYGIFT.MAP_FormInputValue_Services mfiv on iep.EPRefID = mfiv.EPRefID 
+			m.ItemID = iep.DestID -- left join 
+--		x_LEGACYGIFT.MAP_FormInstance_Services mfi on iep.EPRefID = mfi.EPRefID left join 
+--		x_LEGACYGIFT.MAP_FormInstanceInterval_Services mfii on iep.EPRefID = mfii.EPRefID left join 
+--		x_LEGACYGIFT.MAP_FormInputValue_Services mfiv on iep.EPRefID = mfiv.EPRefID 
 GO
 --
 
