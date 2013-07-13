0be3c0170b7b1c27f327b3e15e4276c9058ad004
George Giddens <github@georgegiddens.com>
2013-07-13 08:47:12 -0400

    Get the SectionDefID dynamically.

--- DbScripts/LEGACYSPED/Objects/Transform_PrgConsent.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_PrgConsent.sql
@@ -10,15 +10,16 @@ AS
 		ConsentDate = 
 			case m.DefID 
 				when 'D83A4710-A69F-4310-91F8-CB5BFFB1FE4C' then CAST(isnull(iep.ConsentForServicesDate, iep.StartDate) as DATETIME) -- sped consent services
-				when '47958E63-10C4-4124-A5BA-8C1077FB2D40' then cast(iep.ConsentForEvaluationDate as datetime) -- consent for evaluation 
+				when (select SectionDefID from LEGACYSPED.ImportPrgSections where SectionDefName = 'Sped Consent Evaluation') then cast(iep.ConsentForEvaluationDate as datetime) -- consent for evaluation 
 			end, 
 		iep.DoNotTouch
 	FROM
 		LEGACYSPED.Transform_PrgIep iep JOIN
 		LEGACYSPED.MAP_PrgSectionID_NonVersioned m on 
 			m.DefID in (
-				'D83A4710-A69F-4310-91F8-CB5BFFB1FE4C', --Sped Consent Services
-				'47958E63-10C4-4124-A5BA-8C1077FB2D40' --Sped Consent Evaluation (I have requested VC3 to add this SectionDef to all instance of Enrich in every state with this ID).  gg  20130122
+				--'D83A4710-A69F-4310-91F8-CB5BFFB1FE4C', --Sped Consent Services
+				--'47958E63-10C4-4124-A5BA-8C1077FB2D40' --Sped Consent Evaluation (I have requested VC3 to add this SectionDef to all instance of Enrich in every state with this ID).  gg  20130122
+				select SectionDefID from LEGACYSPED.ImportPrgSections where SectionDefName like '%Consent%'
 				) and
 			m.ItemID = iep.DestID
 GO
