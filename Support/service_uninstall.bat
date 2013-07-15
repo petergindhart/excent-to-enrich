970d9f36a2344744039323b4ee4413bcf0372de9
George Giddens <github@georgegiddens.com>
2013-07-15 11:08:33 -0400

    Moved several files.

--- Support/service_uninstall.bat
+++
@@ -1,5 +0,0 @@
-
-set newsvc="VC3 Scheduled Tasks (DC5)"
-set exe="E:\Sites\Enrich\Excent Enrich DC5 CO Poudre\TasksService\TestViewTasksService.exe"
-
-ExecuteTask\ExecuteTask.exe uninstallservice ServiceExe=%exe% ServiceName=%newsvc%
