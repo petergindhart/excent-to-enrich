fcf8c8b4658d44b3271f6e879aa9ada779d87c13
Muthu <muthuveerappan0104@gmail.com>
2013-07-15 07:10:39 -0600

    Added the script to check AccomMod.csv file specifications.

--- DbScripts/x_LEGACYDOC/Changes/0000-RegisterModule.sql
+++ DbScripts/x_LEGACYDOC/Changes/0000-RegisterModule.sql
@@ -6,9 +6,9 @@ exec VC3Deployment.CreateSchema 'x_LEGACYDOC'
 
 -- Register module
 exec VC3Deployment.AddModule 'x_LEGACYDOC'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='dbo'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='VC3ETL'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='LEGACYSPED'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='x_LEGACY504'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='x_LEGACYGIFT'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='dbo'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='VC3ETL'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='LEGACYSPED'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='x_LEGACY504'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='x_LEGACYGIFT'
 --
--- DbScripts/x_LEGACYGIFT/Changes/0000-RegisterModule.sql
+++ DbScripts/x_LEGACYGIFT/Changes/0000-RegisterModule.sql
@@ -6,9 +6,9 @@ exec VC3Deployment.CreateSchema 'x_LEGACYGIFT'
 
 -- Register module
 exec VC3Deployment.AddModule 'x_LEGACYGIFT'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='dbo'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='VC3ETL'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='LEGACYSPED'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='x_LEGACY504'
-exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='x_LEGACYDOC'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='dbo'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='VC3ETL'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='LEGACYSPED'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='x_LEGACY504'
+--exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='x_LEGACYDOC'
 --
