15f924e7465dd9ea268e6742995fcea5c0c7c9e0
George Giddens <github@georgegiddens.com>
2013-08-03 14:57:06 -0400

    Fixed ModuleDependencies

--- DbScripts/x_LEGACYDOC/Changes/0000-RegisterModule.sql
+++ DbScripts/x_LEGACYDOC/Changes/0000-RegisterModule.sql
@@ -2,13 +2,18 @@
 -- so keep work here to a minimum
 
 -- Create schema
+if not exists (select 1 from sys.schemas where name = 'x_LEGACYDOC')
 exec VC3Deployment.CreateSchema 'x_LEGACYDOC'
 
+
+if not exists (select 1 from VC3Deployment.ModuleDependency where UsedBy = 'x_LEGACYDOC')
+begin
 -- Register module
 exec VC3Deployment.AddModule 'x_LEGACYDOC'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='dbo'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='VC3ETL'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='LEGACYSPED'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='x_LEGACY504'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYDOC', @usedBy='x_LEGACYGIFT'
+exec VC3Deployment.AddModuleDependency @usedBy='x_LEGACYDOC', @uses='dbo'
+exec VC3Deployment.AddModuleDependency @usedBy='x_LEGACYDOC', @uses='VC3ETL'
+exec VC3Deployment.AddModuleDependency @usedBy='x_LEGACYDOC', @uses='LEGACYSPED'
+exec VC3Deployment.AddModuleDependency @usedBy='x_LEGACYDOC', @uses='x_LEGACY504'
+exec VC3Deployment.AddModuleDependency @usedBy='x_LEGACYDOC', @uses='x_LEGACYGIFT'
+end
 --
