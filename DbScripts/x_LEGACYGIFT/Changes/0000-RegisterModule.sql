673e2b35a0ec7a443759f83af8ae2f7deb3a21cc
George Giddens <github@georgegiddens.com>
2013-08-03 14:38:17 -0400

    Change ModuleDependency

--- DbScripts/x_LEGACYGIFT/Changes/0000-RegisterModule.sql
+++ DbScripts/x_LEGACYGIFT/Changes/0000-RegisterModule.sql
@@ -6,9 +6,7 @@ exec VC3Deployment.CreateSchema 'x_LEGACYGIFT'
 
 -- Register module
 exec VC3Deployment.AddModule 'x_LEGACYGIFT'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='dbo'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='VC3ETL'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='LEGACYSPED'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='x_LEGACY504'
---exec VC3Deployment.AddModuleDependency @uses='x_LEGACYGIFT', @usedBy='x_LEGACYDOC'
+exec VC3Deployment.AddModuleDependency @usedBy='x_LEGACYGIFT', @uses='dbo'
+exec VC3Deployment.AddModuleDependency @usedBy='x_LEGACYGIFT', @uses='VC3ETL'
+exec VC3Deployment.AddModuleDependency @usedBy='x_LEGACYGIFT', @uses='LEGACYSPED'
 --
