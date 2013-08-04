7d66f4453fa0b52e70fee84fceab5fa181470a9c
George Giddens <github@georgegiddens.com>
2013-08-04 12:14:19 -0400

    Sarasota's EO data did not have any student identifier that matched Enrich data that was imported from SIS, so we have to match by firstname, lastname and birthdate, where possible.

--- DbScripts/x_LEGACYGIFT/Changes/0001-Extract-GiftedStudent.sql
+++ DbScripts/x_LEGACYGIFT/Changes/0001-Extract-GiftedStudent.sql
@@ -6,9 +6,10 @@ GO
 CREATE TABLE x_LEGACYGIFT.GiftedStudent_LOCAL (
 StudentRefID	varchar(150)	not null,
 EPRefID	varchar(150)	not null,
-StudentID	varchar(20)	not null, -- used for matchine
+StudentID	varchar(20)	not null, -- used for matching
 Firstname	varchar(50)	not null,
 Lastname	varchar(50)	not null,
+Birthdate	datetime	not null,
 EPMeetingDate	datetime	not null,
 LastEPDate	datetime null,
 DurationDate	datetime	not null
@@ -18,6 +19,10 @@ GO
 alter table x_LEGACYGIFT.GiftedStudent_LOCAL 
 add constraint PK_x_LEGACYGIFT_GiftedStudent_LOCAL_StudentRefID primary key (StudentRefID)
 Go
+
+create unique index IX_x_LEGACYGIFT_GiftedStudent_LOCAL_First_Last_DOB on x_LEGACYGIFT.GiftedStudent_LOCAL (Firstname, lastname, birthdate)
+go
+
 
 IF  EXISTS (SELECT 1 FROM sys.views WHERE object_id = OBJECT_ID(N'x_LEGACYGIFT.GiftedStudent'))
 DROP VIEW x_LEGACYGIFT.GiftedStudent
