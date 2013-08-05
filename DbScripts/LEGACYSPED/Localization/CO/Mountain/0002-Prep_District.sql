24cd367a127c1720d9d8d668d50aa3cb3eec8cb0
Muthu <muthuveerappan0104@gmail.com>
2013-08-04 23:07:24 -0600

    Modified.

--- DbScripts/LEGACYSPED/Localization/CO/Mountain/0002-Prep_District.sql
+++ DbScripts/LEGACYSPED/Localization/CO/Mountain/0002-Prep_District.sql
@@ -1,8 +1,10 @@
 --#include ..\..\..\Objects\Transform_ServiceFrequency.sql
 
 -- Colorado
--- Mountain													copied from Ft.LuptonKeensBurg
--- note : data model for SystemSettings table has changed (v 19)
+-- Mountain						copied from Boulder
+
+
+
 if exists (select 1 from sys.schemas s join sys.objects o on s.schema_id = o.schema_id where s.name = 'LEGACYSPED' and o.name = 'MAP_AdminUnitID')
 drop table LEGACYSPED.MAP_AdminUnitID
 go
@@ -19,49 +21,86 @@ go
 
 -- to consider:  in case these get deleted, have code that will insert them if they are not here.  Not necessary at this point.
 declare @OrgUnit table (ID uniqueidentifier, Name varchar(200), Number varchar(10))
-insert @OrgUnit values ('C0EE4341-A386-444A-8485-97209F816C91', 'Weld RE8', '3140')
+INSERT @orgunit VALUES ('52830B77-8AB8-4A4C-A0EB-A7DD0D233012','Aspen School District','2640')
+INSERT @orgunit VALUES ('4BE21F7B-C8B7-4E67-A438-0C54847B437E','Garfield School District 16','1220')
+INSERT @orgunit VALUES ('7FEF1C61-93C8-448D-9129-8FE572FA43FA','Lake School District','1510')
+INSERT @orgunit VALUES ('6531EF88-352D-4620-AF5D-CE34C54A9F53','Mountain BOCES','9030')
+INSERT @orgunit VALUES ('B1DD343F-D7BA-4BA5-9DD2-D69375E6B26B','Park School District RE-2','2610')
+INSERT @orgunit VALUES ('251A4545-F693-4C69-B852-550B1E4A84F7','Summit School District','3000')
+
 
 update ou set Number = t.Number
 -- select * 
 from @OrgUnit t join
 OrgUnit ou on t.ID = ou.ID 
+-- will be zero rows affected
+
+-- select * from PrgStatus where name like '%Conve%'
+if not exists (select 1 from PrgStatus where ID = '0B5D5C72-5058-4BF5-A414-BDB27BD5DD94')
+insert PrgStatus (ID, ProgramID, Sequence, Name, IsExit, IsEntry, StatusStyleID) values ('0B5D5C72-5058-4BF5-A414-BDB27BD5DD94', 'F98A8EF2-98E2-4CAC-95AF-D7D89EF7F80C', 99, 'Converted Data Plan', 0, 0, '85AAB540-503F-4613-9F1F-A14C72764285')
+
+if exists (select 1 from sys.schemas s join sys.objects o on s.schema_id = o.schema_id where s.name = 'LEGACYSPED' and o.name = 'MAP_PrgStatus_ConvertedDataPlan')
+drop table LEGACYSPED.MAP_PrgStatus_ConvertedDataPlan
+go
+
+create table LEGACYSPED.MAP_PrgStatus_ConvertedDataPlan (DestID uniqueidentifier not null)
+insert LEGACYSPED.MAP_PrgStatus_ConvertedDataPlan values ('0B5D5C72-5058-4BF5-A414-BDB27BD5DD94') 
+go
 
 
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'LEGACYSPED.ImportPrgSections') AND type in (N'U'))
-BEGIN
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'LEGACYSPED.ImportPrgSections') AND type in (N'U'))
+DROP TABLE LEGACYSPED.ImportPrgSections
+GO
 CREATE TABLE LEGACYSPED.ImportPrgSections (
 Enabled bit not null,
 SectionDefName varchar(100) not null,
 SectionDefID uniqueidentifier not null
 )
+GO
 
 ALTER TABLE LEGACYSPED.ImportPrgSections
 	ADD CONSTRAINT PK_ImportPrgSections PRIMARY KEY CLUSTERED
 (
 	SectionDefID
 )
-END
 GO
 
+
+if not exists (select 1 from PrgSectionDef where ID = 'F60392DA-8EB3-49D0-822D-77A1618C1DAA')
+insert PrgSectionDef (ID, TypeID, ItemDefID, Sequence, IsVersioned, DisplayPrevious, CanCopy, HeaderFormTemplateID) values ('F60392DA-8EB3-49D0-822D-77A1618C1DAA', '9B10DCDE-15CC-4AA3-808A-DFD51CE91079', '8011D6A2-1014-454B-B83C-161CE678E3D3', 6, 0, 0, 0, 'B97E7849-36B4-4181-8D03-241FDCA5105C')
 set nocount on;
 declare @importPrgSections table (Enabled bit not null, SectionDefName varchar(100) not null, SectionDefID uniqueidentifier not null)
 -- update the Enabled column below to 0 if the section is not required for this district
-insert @importPrgSections values (1, 'IEP Services', '9AC79680-7989-4CC9-8116-1CCDB1D0AE5F')
+insert @importPrgSections values (0, 'IEP Services', '9AC79680-7989-4CC9-8116-1CCDB1D0AE5F') -- boulder has opted not to import converted data services
 insert @importPrgSections values (1, 'IEP LRE', '0CBA436F-8043-4D22-8F3D-289E057F1AAB')
 insert @importPrgSections values (1, 'IEP Dates', 'EE479921-3ECB-409A-96D7-61C8E7BA0E7B')
 insert @importPrgSections values (1, 'IEP Demographics', '427AF47C-A2D2-47F0-8057-7040725E3D89')
 insert @importPrgSections values (1, 'Sped Eligibility Determination', 'F050EF5E-3ED8-43D5-8FE7-B122502DE86A')
 insert @importPrgSections values (1, 'IEP Goals', '84E5A67D-CC9A-4D5B-A7B8-C04E8C3B8E0A')
 insert @importPrgSections values (1, 'Sped Consent Services', 'D83A4710-A69F-4310-91F8-CB5BFFB1FE4C')
+insert @importPrgSections values (1, 'Sped Consent Evaluation', '0FEB4F39-9450-43A4-BF09-A98C4D296916') -- BOULDER DID NOT PROVIDE THIS DATE!
+insert @importPrgSections values (1, 'IEP ESY', 'F60392DA-8EB3-49D0-822D-77A1618C1DAA')
 
 insert LEGACYSPED.ImportPrgSections
-select t.* 
-from @importPrgSections t left join
-LEGACYSPED.ImportPrgSections p on t.SectionDefID = p.SectionDefID
-where p.SectionDefID is null
+select * from @importPrgSections where SectionDefID not in (select SectionDefID from LEGACYSPED.ImportPrgSections)
 go
-
-
+---- #############################################################################
+----		SUB Goal Area MAP
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'LEGACYSPED.MAP_IepSubGoalAreaDefID') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE LEGACYSPED.MAP_IepSubGoalAreaDefID 
+(
+	SubGoalAreaCode	varchar(150) NOT NULL,
+	DestID uniqueidentifier NOT NULL,
+	ParentID uniqueidentifier not null
+)
+ALTER TABLE LEGACYSPED.MAP_IepSubGoalAreaDefID ADD CONSTRAINT
+PK_MAP_IepSubGoalAreaDefID PRIMARY KEY CLUSTERED
+(
+	SubGoalAreaCode
+)
+END
+go
 
 -- Map_ServiceFrequencyID is created in the Transform script.
 
@@ -73,6 +112,24 @@ go
 			i.e. 2 times Quarterly = 8 times yearly,  30 minutes per quarter = 2 hours per year or 120 minutes per year
 */
 
+--		Service Frequency
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'LEGACYSPED.MAP_ServiceFrequencyID') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE LEGACYSPED.MAP_ServiceFrequencyID
+(
+	ServiceFrequencyCode	varchar(150) NOT NULL,
+	ServiceFrequencyName	varchar(50) not null,
+	DestID uniqueidentifier NOT NULL
+)
+ALTER TABLE LEGACYSPED.MAP_ServiceFrequencyID ADD CONSTRAINT
+PK_MAP_ServiceFrequencyID PRIMARY KEY CLUSTERED
+(
+	ServiceFrequencyName
+)
+CREATE INDEX IX_Map_ServiceFrequencyID_ServiceFrequencyName on LEGACYSPED.Map_ServiceFrequencyID (ServiceFrequencyName)
+END
+GO
+
 declare @map_servicefrequencyid table (servicefrequencycode varchar(30), servicefrequencyname varchar(50), destid uniqueidentifier)
 set nocount on;
 insert @map_servicefrequencyid values ('zzz', 'not specified', 'c42c50ed-863b-44b8-bf68-b377c8b0fa95')
@@ -83,10 +140,7 @@ insert @map_servicefrequencyid values ('', 'yearly', '5f3a2822-56f3-49da-9592-f6
 insert @map_servicefrequencyid values('an', 'as needed', '69439d9d-b6c1-4b7a-9cac-c69810adfd31')
 insert @map_servicefrequencyid values('esy', 'as needed for esy', '836d1e97-ce4d-4fd5-9d0a-148924ac007b')
 
-
--- select * from ServiceFrequency
-
-if (select count(*) from @map_servicefrequencyid t join legacysped.map_servicefrequencyid m on t.destid = m.destid) <> 5
+if (select count(*) from @map_servicefrequencyid t join legacysped.map_servicefrequencyid m on t.destid = m.destid) <> 5 ------ what is the purpose of this?
 	delete legacysped.map_servicefrequencyid
 
 set nocount off;
@@ -131,3 +185,4 @@ as
 -- this should run for all districts in all states
 update d set IsReevaluationNeeded = 1, StartDate = dateadd(dd, -d.MaxDaysToComplete, dateadd(yy, -d.MaxYearsToComplete, getdate())) from PrgMilestoneDef d where d.ID in ('27C002AF-ED92-4152-8B8C-7CA1ADEA2C81', 'AC043E4C-55EC-4F10-BCED-7E9201D7D0E2')
 GO
+
