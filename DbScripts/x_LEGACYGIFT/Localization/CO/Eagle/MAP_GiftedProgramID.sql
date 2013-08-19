3d0ba075bca54ba42b954bc347d619d15e428b93
George Giddens <github@georgegiddens.com>
2013-08-19 02:41:52 -0400

    Copied Moffat Gifted

--- DbScripts/x_LEGACYGIFT/Localization/CO/Eagle/MAP_GiftedProgramID.sql
+++ DbScripts/x_LEGACYGIFT/Localization/CO/Eagle/MAP_GiftedProgramID.sql
@@ -2,7 +2,22 @@ IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'x_LEGACYGIFT.
 DROP TABLE x_LEGACYGIFT.MAP_GiftedProgramID
 GO
 
-create table x_LEGACYGIFT.MAP_GiftedProgramID (DestID uniqueidentifier not null)
--- insert x_LEGACYGIFT.MAP_GiftedProgramID values ('2FF58E06-9E4A-4BE5-8274-E0FDE0012D4E') -- this is a dummy record for Boulder.  No gifted here
-go
+create table x_LEGACYGIFT.MAP_GiftedProgramID (
+	DestID uniqueidentifier not null, 
+	VariantID uniqueidentifier not null, 
+	ConvertedEPID uniqueidentifier not null
+)
+--insert x_LEGACYGIFT.MAP_GiftedProgramID values (
+--	NULL, -- Gifted Program
+--	NULL, -- VariantID
+--	NULL -- Converted EP
+--)
+--go
 
+/*
+
+select * from Program
+select * from PrgVariant where ProgramID = ''
+select * from PrgItemDef where ProgramID = ''
+
+*/
