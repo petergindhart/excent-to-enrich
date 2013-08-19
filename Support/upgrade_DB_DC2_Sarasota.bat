19898525d1b0167ae592c354c965f19ae6752c63
George Giddens <github@georgegiddens.com>
2013-08-19 03:28:55 -0400

    Insignificant changes.

--- Support/upgrade_DB_DC2_Sarasota.bat
+++ Support/upgrade_DB_DC2_Sarasota.bat
@@ -5,5 +5,5 @@ set connection=server=.;uid=enrich_db_user;pwd=vc3go!!;database=Enrich_DC2_FL_Sa
 echo DB Upgrade %state% %speddistrict%
 ExecuteTask\ExecuteTask.exe project projectfile="upgrade_db_project.xml" $connection="%connection%" $state="%state%" $speddistrict="%speddistrict%"
 
-net stop "VC3 TestView Scheduled Tasks (DevDC2)"
-net start "VC3 TestView Scheduled Tasks (DevDC2)"
+rem net stop "VC3 TestView Scheduled Tasks (DevDC2)"
+rem net start "VC3 TestView Scheduled Tasks (DevDC2)"
