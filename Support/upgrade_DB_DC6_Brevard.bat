b057503378e86f4aaf1d1e9a0e3261d83eb2f441
George Giddens <github@georgegiddens.com>
2013-10-01 07:28:06 -0400

    minor changes

--- Support/upgrade_DB_DC6_Brevard.bat
+++ Support/upgrade_DB_DC6_Brevard.bat
@@ -5,5 +5,5 @@ set connection=server=.;uid=enrich_db_user;pwd=vc3go!!;database=Enrich_DC6_FL_Br
 echo DB Upgrade %state% %speddistrict%
 ExecuteTask\ExecuteTask.exe project projectfile="upgrade_db_project.xml" $connection="%connection%" $state="%state%" $speddistrict="%speddistrict%"
 
--- net stop "VC3 TestView Scheduled Tasks (DevDC6)"
--- net start "VC3 TestView Scheduled Tasks (DevDC6)"
+net stop "VC3 TestView Scheduled Tasks (DevDC6)"
+net start "VC3 TestView Scheduled Tasks (DevDC6)"
