5e1e4738ce69ed12245ee65a310ee47ed88fcc5e
George Giddens <github@georgegiddens.com>
2013-11-18 08:59:51 -0500

    Handle importing same user multiple times.

--- DbScripts/LEGACYSPED/Objects/Transform_UserProfile.sql
+++ DbScripts/LEGACYSPED/Objects/Transform_UserProfile.sql
@@ -6,8 +6,13 @@ CREATE VIEW LEGACYSPED.Transform_UserProfile
 as
 SELECT DestID = isnull(upn.ID, m.DestID), 
 	RoleID = isnull(r.ID, 'A101B7EC-62CA-48C2-9562-DD511AB88534'),  --NoAccess
-	UserName = 'Enrich:'+sm.Firstname+ sm.Lastname, CanPerformAllServices = cast(0 as bit), CanSignAllServices = cast(0 as bit), IsSchoolAutoSelected = cast(0 as bit), CurrentFailedLoginAttempts = cast (0 as int), RoleStatusID = 'M'
-	-- select sm.*, up.*
+	UserName = 'Enrich:'+sm.Firstname+ sm.Lastname+cast(case when (select count(*)+1 from LEGACYSPED.SPEDStaffMember dt where sm.Firstname = dt.Firstname and sm.Lastname = dt.Lastname and sm.StaffEmail < dt.StaffEmail) > 1 then cast((select count(*)+1 from LEGACYSPED.SPEDStaffMember dt where sm.Firstname = dt.Firstname and sm.Lastname = dt.Lastname and sm.StaffEmail < dt.StaffEmail) as varchar(2)) else '' end as varchar(2)),
+	CanPerformAllServices = cast(0 as bit), 
+	CanSignAllServices = cast(0 as bit), 
+	IsSchoolAutoSelected = cast(0 as bit), 
+	CurrentFailedLoginAttempts = cast (0 as int), 
+	RoleStatusID = 'M'
+	-- select sm.*
 FROM LEGACYSPED.SPEDStaffMember sm  join 
 	LEGACYSPED.MAP_PersonID m ON m.StaffEmail = sm.StaffEmail left join
 	UserProfile up ON up.ID = m.DestID left join 
