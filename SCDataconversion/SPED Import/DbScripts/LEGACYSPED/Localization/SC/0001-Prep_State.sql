353cfcabe6910371f308ed267104d71aaeaa0ca2
Muthu <muthuveerappan0104@gmail.com>
2014-02-11 08:39:17 -0700

    Modified for state code  mapping

--- SCDataconversion/SPED Import/DbScripts/LEGACYSPED/Localization/SC/0001-Prep_State.sql
+++ SCDataconversion/SPED Import/DbScripts/LEGACYSPED/Localization/SC/0001-Prep_State.sql
@@ -5,9 +5,28 @@
 
 */
 
--- Ensure GradeLevel StateCode is populated.  Specific to Colorado
+-- Ensure GradeLevel StateCode is populated.  Specific to South Carolina
 update gl set StateCode = case when Name like 'K%' then 'KG' when Name = '00' then NULL else Name end from GradeLevel gl where (ISNUMERIC(Name) = 1 or Name in ('K', 'PK', 'KG'))
 go
+
+
+--	01	01
+--	02	02
+--	03	03
+--	04	04
+--	05	05
+--	06	06
+--	07	07
+--	08	08
+--	09	09
+--	10	10
+--	11	11
+--	12	12
+--	AE	AE
+--	KG	Kindergarten
+--	PK	Pre-Kindergarten
+
+--select * from GradeLevel order by StateCode
 
 
 -- NEW powerschool map to indicate up to how many leading zeros to add to district and school numbers
@@ -23,9 +42,9 @@ CREATE TABLE LEGACYSPED.DistrictSchoolLeadingZeros
 	Zeros	varchar(10) not null
 )
 
--- MI:  District 5 characters, School 5 characters
-insert LEGACYSPED.DistrictSchoolLeadingZeros values ('District', '00000')
-insert LEGACYSPED.DistrictSchoolLeadingZeros values ('School', '00000')
+-- SC:  District 4 characters, School 3 characters
+insert LEGACYSPED.DistrictSchoolLeadingZeros values ('District', '0000')
+insert LEGACYSPED.DistrictSchoolLeadingZeros values ('School', '000') -- I think it's 3
 go
 
 
@@ -42,25 +61,187 @@ go
 */
 set nocount on;
 declare @Map_IepDisabilityID table (StateCode varchar(10), DestID uniqueidentifier)
-insert @Map_IepDisabilityID values ('13 ', '8D9B3B54-4080-4D5E-A196-956D5223E479')
---insert @Map_IepDisabilityID values ('09 ', '')
-insert @Map_IepDisabilityID values ('05 ', '79450790-72A4-4F16-8840-6193DB199A1E')
---insert @Map_IepDisabilityID values ('12 ', '')
-insert @Map_IepDisabilityID values ('10 ', 'CA41A561-16BE-4E21-BE8A-BC59ED86C921')
-insert @Map_IepDisabilityID values ('07 ', '07093979-0C3F-414D-9750-8080C6BB7C45')
-insert @Map_IepDisabilityID values ('11 ', 'B0439371-3A5E-4FD7-912E-0C3AA4963C26')
-insert @Map_IepDisabilityID values ('03 ', 'A1504419-19F6-434B-B4A3-1E5A69E99A9B')
-insert @Map_IepDisabilityID values ('01 ', '8D0AA58F-597A-462F-B509-BB8F0B2AB593')
-insert @Map_IepDisabilityID values ('04 ', '0E026822-6B22-43A1-BD6E-C1412E3A6FA3')
-insert @Map_IepDisabilityID values ('08 ', '96A66C19-7AAB-4EE6-AA81-7CD8CD4FEB09')
-insert @Map_IepDisabilityID values ('14 ', 'E65E6AAE-AE9B-4275-B547-9CE1A4B9EEF3')
-insert @Map_IepDisabilityID values ('06 ', 'D31E4ED0-9A37-490F-B49B-FF18133644FE')
+--
+insert @Map_IepDisabilityID values ('SDE01', '79450790-72A4-4F16-8840-6193DB199A1E')
+insert @Map_IepDisabilityID values ('SDE13', '555B1135-5B09-419D-9DA7-C7F316B71A3B')
+insert @Map_IepDisabilityID values ('SDE02', '52431290-0B16-4732-840B-10986A1FBE1E')
+insert @Map_IepDisabilityID values ('SDE11', 'AC7D83DA-B896-4A27-A983-A48243819BC6')
+insert @Map_IepDisabilityID values ('SDE15', '6ACBABEA-F128-4D52-A7C4-9478A9B9FACB')
+insert @Map_IepDisabilityID values ('SDE05', 'A1504419-19F6-434B-B4A3-1E5A69E99A9B')
+insert @Map_IepDisabilityID values ('SDE12', '902770E8-02EE-4538-B6F4-1BB1A691A131')
+insert @Map_IepDisabilityID values ('SDE06', '607943A6-7CFA-4BD1-9044-6A3634AB1ED5')
+insert @Map_IepDisabilityID values ('SDE07', '3567F202-646F-4A7E-860A-2100242C7182')
+insert @Map_IepDisabilityID values ('SDE10', '2F8DD08D-00C6-4C23-859E-718B1E347722')
+insert @Map_IepDisabilityID values ('SDE03', '5CEAB474-9E65-4190-82CA-C71387C3B03B')
+insert @Map_IepDisabilityID values ('SDE14', 'F1A19B97-8A22-4498-9E44-C07AB3963736')
+insert @Map_IepDisabilityID values ('SDE04', 'D31E4ED0-9A37-490F-B49B-FF18133644FE')
 
 set nocount off;
 update d set StateCode = m.StateCode
 from @Map_IepDisabilityID m
 join IepDisability d on m.DestID = d.ID
 go
+
+
+-- (comments after the insert line are the ServiceFrequency description from EO)
+set nocount on;
+declare @servfreq table (ID uniqueidentifier, StateCode varchar(20), Name varchar(max),sequence int,weekfactor float)
+
+insert @servfreq values ('E2996A26-3DB5-42F3-907A-9F251F58AB09', 'SDE01', 'only once',4,0.02) -- only once
+insert @servfreq values ('C9DFF9A1-6CC2-4900-BA7B-3F94833C0F86', 'SDE02', 'Twice',5,1) -- Twice
+insert @servfreq values ('3AE42C6C-8B3C-4CFD-B24A-323A549F5702', 'SDE03', 'Three Times',6,1) -- Three Times
+insert @servfreq values ('69DEA0B9-F8F0-4516-8167-FB8876ED9C83', 'SDE04', 'Quarterly',7,1) -- Quarterly
+insert @servfreq values ('3D4B557B-0C2E-4A41-9410-BA331F1D20DD', 'SDE05', 'monthly',2,0.23) -- monthly
+insert @servfreq values ('A2080478-1A03-4928-905B-ED25DEC259E6', 'SDE06', 'weekly',1,1) -- weekly
+insert @servfreq values ('41A951D7-ED35-4AF4-945D-89F228624014', 'SDE07', 'Biweekly',8,1) -- Biweekly
+insert @servfreq values ('71590A00-2C40-40FF-ABD9-E73B09AF46A1', 'SDE08', 'daily',0,5) -- daily
+insert @servfreq values ('9DE6A916-BC39-47BB-8D2B-15EDEC5C5FE3', 'SDE09', 'Consultation',9,1) -- Consultation
+insert @servfreq values ('5F3A2822-56F3-49DA-9592-F604B0F202C3', NULL, 'yearly',3,0.02) -- yearly
+
+
+--select * From @servfreq
+set nocount off;
+UPDATE sfq
+SET StateCode = tfq.StateCode
+--SELECT tfq.* 
+FROM @servfreq tfq 
+JOIN ServiceFrequency sfq ON sfq.ID = tfq.ID
+
+
+insert ServiceFrequency table (ID , StateCode , Name ,sequence ,weekfactor)
+SELECT tfq.ID,tfq.StateCode,tfq.Name,tfq. 
+FROM @servfreq tfq 
+LEFT JOIN ServiceFrequency sfq ON sfq.ID = tfq.ID
+WHERE sfq.ID IS NULL 
+
+
+--PrgLocation (ServiceLocationCode)
+DECLARE @prgloc table (ID uniqueidentifier, Name varchar(max),StateCode varchar(20))
+INSERT @prgloc VALUES ('7A691C77-B4D6-4D4D-8A29-131FC1E7A33A','Home','12')
+INSERT @prgloc VALUES ('465C097B-DEC0-4E20-ACDC-2ACF9E7F5DEF','Therapy Room',NULL)
+INSERT @prgloc VALUES ('2B72B8C6-01DC-47C5-8623-2F230225B300','School Environment','03')
+INSERT @prgloc VALUES ('2087ECC6-914C-486B-81C1-4A1C0A6F0552','Special Education Support Room',NULL)
+INSERT @prgloc VALUES ('B1DA5BF5-325B-496F-A0B2-4AEAA6085C64','Behavior Health Counselor Room',NULL)
+INSERT @prgloc VALUES ('F41868AE-61D1-48A7-8CE0-58B69E5725BD','Cafeteria',NULL)
+INSERT @prgloc VALUES ('52C74FE7-5685-4F8C-AAF2-63B40A8E4B51','Special Education Classroom',NULL)
+INSERT @prgloc VALUES ('6102B96C-1549-4C65-AD59-683FF00639BA','Occupational Therapy Room',NULL)
+INSERT @prgloc VALUES ('27D86DFE-218E-4E1B-A6DB-8B8E74D6F8BA','General Education Classroom',NULL)
+INSERT @prgloc VALUES ('87A9F34F-E5E3-414B-A3B9-91273AD606DF','Physical Therapy Room',NULL)
+INSERT @prgloc VALUES ('E9DD5433-BC4C-4817-BF1D-A0B9203BAB8B','Nurses Room',NULL)
+INSERT @prgloc VALUES ('2D48D839-511D-4CBA-9E72-BDE1348EDCFB','Bus/District Transportation',NULL)
+INSERT @prgloc VALUES ('AA221ED5-CCB4-4A76-B0C6-D0A2B7E2835E','Speech Therapy Room',NULL)
+INSERT @prgloc VALUES ('701DF30A-7C66-423D-9796-DE5B5CB97139','Guidance Counselor Room',NULL)
+INSERT @prgloc VALUES ('1BE8AF70-A842-4ECF-9C50-E67C1F9A80C7','Vocational Rehabilitation Workshop',NULL)
+INSERT @prgloc VALUES ('8FC37445-260F-4185-8E43-F5EC8AFCDDB3','Community',NULL)
+
+--SELECT * FROM @prgloc
+
+
+UPDATE ploc
+SET StateCode = tloc.StateCode
+--SELECT tloc.* 
+FROM @prgloc tloc 
+JOIN PrgLocation ploc ON ploc.ID = tloc.ID
+
+INSERT PrgLocation (ID,Name,StateCode)
+SELECT tloc.ID,tloc.Name,tloc.StateCode
+FROM @prgloc tloc 
+LEFT JOIN PrgLocation ploc ON ploc.ID = tloc.ID
+WHERE ploc.ID IS NULL
+
+
+
+DECLARE @servdef table (ID uniqueidentifier,TypeID uniqueidentifier,Name varchar(max),StateCode varchar(20),MinutesPerUnit int,UserDefined int )
+
+INSERT @servdef VALUES ('8C054380-B22F-4D2A-98DE-568498E06EAB','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Assistive Technology Services','SDE01',15,1)
+INSERT @servdef VALUES ('79969098-F7DC-4280-93AF-73658279F945','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Behavioral Health Screening',NULL,15,1)
+INSERT @servdef VALUES ('9DE4CBF9-BD8D-490C-8E1B-34F5E73DEF11','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Classroom Instruction',NULL,15,1)
+INSERT @servdef VALUES ('CD96747D-3D67-4207-86FD-FD754A498102','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Community Support Services',NULL,15,1)
+INSERT @servdef VALUES ('F6C60A4E-774E-4AE1-80F4-70848B52F846','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Crisis Management',NULL,15,1)
+INSERT @servdef VALUES ('F71E1EB1-1322-4AA4-9267-F8039F888D74','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Follow-up Comprehensive Assessment',NULL,15,1)
+INSERT @servdef VALUES ('3427C7E6-0501-4860-8055-D284524BEEC7','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Initial Comprehensive Assessment',NULL,15,1)
+INSERT @servdef VALUES ('B874A136-2F0E-4955-AA1E-1F0D45F263FB','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Occupational Therapy','SDE05',15,1)
+INSERT @servdef VALUES ('73107912-4959-4137-910B-B17E52076074','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Physical Therapy','SDE07',15,1)
+INSERT @servdef VALUES ('7BBAAB01-398D-4835-B4B0-13D543FAC564','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Psychological Services',NULL,60,1)
+INSERT @servdef VALUES ('75D07F63-F586-4C55-8FDE-A5B6D0737157','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','School Health Services',NULL,15,1)
+INSERT @servdef VALUES ('FF86821E-A810-4C91-B8FE-240D96278AAB','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Service Plan Development',NULL,15,1)
+INSERT @servdef VALUES ('A7E0C2B7-2CA5-4838-BF7E-23E05CCB6564','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Therapy Services',NULL,15,1)
+INSERT @servdef VALUES ('003CF444-D485-43B9-8508-8D3B7E27FCB4','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Transportation','SDE10',0,0)
+INSERT @servdef VALUES ('B630AE87-E461-4DAC-B5B9-3FB85C78F56D','D3945E9D-AA0E-4555-BCB2-F8CA95CC7784','Transportation Services',NULL,15,1)
+
+select * from @servdef
+/*
+SDE02	Audiological Services
+SDE04	Nursing Services
+SDE06	Orientation and Mobility
+*/
+
+UPDATE sd
+SET StateCode = tsd.StateCode
+--SELECT tloc.* 
+FROM @servdef tsd 
+JOIN ServiceDef sd ON sd.ID = tsd.ID
+
+INSERT servicedef(ID,TypeID,Name,StateCode,MinutesPerUnit ,UserDefined )
+SELECT tsd.ID,tsd.TypeID,tsd.Name,tsd.StateCode,tsd.MinutesPerUnit ,tsd.UserDefined
+FROM @servdef tsd 
+LEFT JOIN ServiceDef sd ON tsd.ID = sd.ID
+WHERE sd.ID IS NULL
+--
+
+-- for future projects use this code to create table variable insert statements to update manually
+
+--select t.Name, o.Sequence, 'insert @p values ('''+convert(varchar(36), o.ID)+''', ' + isnull(o.StateCode,''''', ''')+ o.Text+''')'
+--from IepPlacementType t 
+--join IepPlacementOption o on t.ID = o.TypeID
+--order by t.name, o.Text
+
+------------------ NOTE:  Received the LRE information from Joe Tebaldi, who received it from the state of SC DOE
+
+-- (comments after the insert line are the LRE description from EO)
+declare @p table (ID uniqueidentifier, StateCode varchar(20), Text varchar(max))
+-- PK 
+insert @p values ('0980382F-594C-453F-A0C9-77D54A0443B1', '7', 'Home') -- In Neither a Regular Early Childhood Program Nor a Special Education Program - Home
+insert @p values ('DC20B53C-0559-44F6-A463-3A92D9D4F69A', '9', 'In Regular Education at least 10 hrs per week - EC Program') -- In Regular Education at Least 10 HRS Per WK - Regular Early Childhood Program
+insert @p values ('E5741B2C-CE35-4F3B-8AD5-58774E31C531', '10', 'In Regular Education at least 10 hrs per week - Other Location') -- In Regular Education at Least 10 HRS Per WK - Other Location
+insert @p values ('A3FA6E17-E828-4A7C-A002-0A49B834BD1E', '11', 'In Regular Education less than 10 hrs per week - EC Program') -- In Regular Education Less Than 10 HRS Per WK - Regular Early Childhood Program
+insert @p values ('4F03529D-9E60-4D76-8E0B-DACD3D207768', '12', 'In Regular Education less than 10 hrs per week - Other Location') -- In Regular Education Less Than 10 HRS Per WK - Other Location
+insert @p values ('0DA48AA5-183C-4434-91C1-AC3C9941BE15', '6', 'Residential Facility') -- In Special Education Program Only - Residential Facility
+insert @p values ('0B2E63D7-6493-44A7-95B1-8DF327D77C38', '4', 'Separate Class') -- In Special Education Program Only - Separate Class
+insert @p values ('2E45FDA2-0767-43D0-892D-D1BB40AFCEC1', '5', 'Separate School') -- In Special Education Program Only - Separate School
+insert @p values ('1945D36A-8D62-4FDB-9F22-5836F553A958', '8', 'Service Provider Location') -- In Neither a Regular Early Childhood Program Nor a Special Education Program - Service Provider Location or Other Location
+-- K12
+insert @p values ('84DAF081-F700-4F57-99DA-A2A983FDE919', '9', 'Correctional Facilities') -- Correctional Facilities
+insert @p values ('521ACE5E-D04B-4E30-80E3-517516383536', '2', 'Inside Regular Class and activities 79-40% of the day') -- Inside Regular Class 79-40% of the day
+insert @p values ('FEFF9910-F320-4097-AFC2-A3D9713470BD', '1', 'Inside Regular Class and activities 80% or more of the day') -- Inside Regular Class 80% or more of the day
+insert @p values ('9CD2726E-6461-4F6C-B65F-B4232FB4D36E', '3', 'Inside Regular Class and activities less than 40% of the day') -- Inside Regular Class For Less Than 40% Of Day
+insert @p values ('304AEBA5-3162-4B40-89D3-F094602CFF2D', '10', 'Parentally Placed In Private Schools') -- Parentally Placed In Private Schools
+insert @p values ('E4EE85F2-8307-4C8D-BA77-4EB5D12D8470', '6', 'Residential Facility') -- Residential Facility
+insert @p values ('77E0EE80-143B-41E5-84B9-5076605CCC9A', '4', 'Separate School') -- Separate School
+-- Homebound/Hospital
+insert @p values ('D634CD6A-C22F-4B34-89A8-340A13891E24', '8c', 'Homebound/Hospital - Home-based')
+insert @p values ('5EE0CA16-1F59-4BC8-9AFA-BAED97D29B77', '8b', 'Homebound/Hospital - Hospital')
+insert @p values ('91EF0ECE-A770-4D05-8868-F19180A000DB', '8a', 'Homebound/Hospital - Medical Homebound')
+
+---- 
+--select UsageID, IEPCode, PlacementDesc
+--from [10.0.1.8\SQLServer2005].QASCConvert2005.dbo.LK_LREPlacements 
+--where UsageID <> 'LREplaceHosp' 
+--and (UsageID = 'LREplace') -- or (UsageID = 'LREPlacePK' and IEPCode not in ('1', '2', '3'))) 
+--order by UsageID Desc, PlacementDesc
+
+
+--select p.AgeGroup, p.LREPlacement, p.HomeHosPlace, k.PlacementDesc, count(*) tot
+--from [10.0.1.8\SQLServer2005].QASCConvert2005.dbo.ReportICIEPLRETblPlace p
+--join [10.0.1.8\SQLServer2005].QASCConvert2005.dbo.LK_LREPlacements k on p.HomeHosPlace = k.IEPCode and k.USageID = 'LREplaceHosp'
+--where HomeHosPlace <> 0
+--group by p.AgeGroup, p.LREPlacement, p.HomeHosPlace, k.PlacementDesc
+
+
+--select * from [10.0.1.8\SQLServer2005].QASCConvert2005.dbo.LK_LREPlacements 
+
+--select * from [10.0.1.8\SQLServer2005].QASCConvert2005.sys.objects where name like 'Report%Place%'
+
 
 -- this may need to be modified for states that don't have a specific code for each race, but set a flag for it (such as FL).  Eather that, or we can derive a code for those statues (first 2 letters of the fed name?)
 IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'LEGACYSPED.MAP_FederalRace') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
@@ -85,6 +266,9 @@ GO
 --insert LEGACYSPED.MAP_FederalRace values ('W', 'White')
 --insert LEGACYSPED.MAP_FederalRace values ('B', 'Black African American')
 go
+
+-- select * from EnumValue where type = 'CBB84AE3-A547-4E81-82D2-060AA3A50535' and isactive = 1
+
 
 -- #############################################################################
 --		Goal Area Def MAP
