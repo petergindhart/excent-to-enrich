64c7796afe66e748e4aca17fe7e35238f35cd56f
George Giddens <github@georgegiddens.com>
2014-02-12 11:58:31 -0500

    Saving my work before a pull.

--- SCDataconversion/SPED Import/DbScripts/LEGACYSPED/Localization/SC/Transform_PrgGoalSubGoalAreaDef.sql
+++ SCDataconversion/SPED Import/DbScripts/LEGACYSPED/Localization/SC/Transform_PrgGoalSubGoalAreaDef.sql
@@ -43,11 +43,49 @@ as
 	where  GAOther = 'Y'
 	--order by GoalRefID, GoalIndex
 	
-	CREATE A DUMMY VIEW FOR STATES THAT DO NOT USE SUBGOALAREAS SO THAT WE CAN RE-USE THE REST OF THE CODE. */
-select top 1 IepRefID, GoalRefID, 'do not need this view in CO' GoalAreaCode, CAST(0 as int) SubGoalDefIndex
-from LEGACYSPED.Goal
-where 0 = 1
+	CREATE A DUMMY VIEW FOR STATES THAT DO NOT USE SUBGOALAREAS SO THAT WE CAN RE-USE THE REST OF THE CODE. 
+*/
+	select IepRefID, GoalRefID, 'SCInstructional' GoalAreaCode, CAST(0 as int) SubGoalDefIndex
+	from LEGACYSPED.Goal
+	where SCInstructional = 'Y'
+	UNION ALL
+	select IepRefID, GoalRefID, 'SCTransition' GoalAreaCode, CAST(1 as int) SubGoalDefIndex
+	from LEGACYSPED.Goal
+	where  SCTransition = 'Y'
+	UNION ALL
+	select IepRefID, GoalRefID, 'SCRelatedService' GoalAreaCode, CAST(2 as int) SubGoalDefIndex
+	from LEGACYSPED.Goal
+	where  SCRelatedService = 'Y'
 go
+
+/*
+
+manufacture some data for testing (currently all columns are N
+
+set nocount on;
+declare @t table (RowNum int not null identity(0,1), SCInstructional char(1), SCTransition char(1), SCRelatedService char(1))
+insert @t (SCInstructional, SCTransition, SCRelatedService) values ('Y', 'N', 'N')
+insert @t (SCInstructional, SCTransition, SCRelatedService) values ('Y', 'Y', 'N')
+insert @t (SCInstructional, SCTransition, SCRelatedService) values ('Y', 'Y', 'Y')
+insert @t (SCInstructional, SCTransition, SCRelatedService) values ('Y', 'N', 'Y')
+insert @t (SCInstructional, SCTransition, SCRelatedService) values ('N', 'Y', 'N')
+insert @t (SCInstructional, SCTransition, SCRelatedService) values ('N', 'Y', 'Y')
+insert @t (SCInstructional, SCTransition, SCRelatedService) values ('N', 'N', 'Y')
+insert @t (SCInstructional, SCTransition, SCRelatedService) values ('N', 'N', 'N')
+
+update g set SCInstructional = t.SCInstructional, SCTransition = t.SCTransition, SCRelatedService = t.SCRelatedService
+-- select * 
+from LEGACYSPED.Goal_LOCAL g
+join @t t on g.GoalRefID % 7 = RowNum
+
+select * from LEGACYSPED.Goal
+
+
+*/
+
+
+
+
 
 IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'LEGACYSPED.Transform_PrgGoalSubGoalAreaDef') AND OBJECTPROPERTY(id, N'IsView') = 1)
 DROP VIEW LEGACYSPED.Transform_PrgGoalSubGoalAreaDef
@@ -64,5 +102,10 @@ LEGACYSPED.Transform_PrgGoal pg on ga.GoalRefID = pg.GoalRefID join
 LEGACYSPED.SubGoalAreaPivotView sg on ga.GoalRefID = sg.GoalRefID left join 
 LEGACYSPED.MAP_IepSubGoalAreaDefID m on sg.GoalAreaCode = m.SubGoalAreaCode 
 --left join PrgGoalSubGoalAreaDef sgad on ga.DestID = sgad.GoalID and m.DestID = sgad.DefID
-where ga.GoalAreaCode = 'GACurriculum' -- assuming only one parent for now
+--where ga.GoalAreaCode = 'GACurriculum' -- assuming only one parent for now
 go
+
+select * from LEGACYSPED.MAP_IepSubGoalAreaDefID m
+
+select * from IepGoalPostSchoolAreaDef
+
