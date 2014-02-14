6f5f29fe9d602d18c3a71a246cbafc151cfe4f28
George Giddens <github@georgegiddens.com>
2014-02-14 10:25:01 -0500

    no change

--- SCDataconversion/ExtractScripts_For_EO/DataConversionFrequencyCodeView.sql
+++ SCDataconversion/ExtractScripts_For_EO/DataConversionFrequencyCodeView.sql
@@ -1,3 +1,192 @@
+
+
+
+if exists (select 1 from sys.objects where name = 'DataConversionFrequencyCode')
+drop table DataConversionFrequencyCode
+go
+
+create table DataConversionFrequencyCode (
+SDECode	varchar(100) not null,
+SDEDesc	varchar(100) not null,
+FuzzyDesc varchar(100) not null
+)
+go
+
+alter table DataConversionFrequencyCode 
+	add constraint PK_DataConversionFrequencyCode_Code primary key (SDECode)
+go
+
+create index IX_DataConversionFrequencyCode_SDEDesc on DataConversionFrequencyCode (SDEDesc)
+go
+
+set nocount on;
+insert DataConversionFrequencyCode values ('SDE01', 'Twice Daily', 'Twice Daily')
+insert DataConversionFrequencyCode values ('SDE02', 'Alternating days as per schedule', 'Alternat% days%')
+insert DataConversionFrequencyCode values ('SDE03', 'Twice weekly', 'Twice weekly')
+insert DataConversionFrequencyCode values ('SDE04', 'Three times per week', '[Three|3] times per week')
+insert DataConversionFrequencyCode values ('SDE05', '', '')
+insert DataConversionFrequencyCode values ('SDE06', '', '')
+insert DataConversionFrequencyCode values ('SDE07', '', '')
+insert DataConversionFrequencyCode values ('SDE08', '', '')
+insert DataConversionFrequencyCode values ('SDE09', '', '')
+insert DataConversionFrequencyCode values ('SDE10', '', '')
+insert DataConversionFrequencyCode values ('SDE11', '', '')
+insert DataConversionFrequencyCode values ('SDE12', '', '')
+insert DataConversionFrequencyCode values ('SDE13', '', '')
+insert DataConversionFrequencyCode values ('SDE14', '', '')
+insert DataConversionFrequencyCode values ('SDE15', '', '')
+insert DataConversionFrequencyCode values ('SDE16', '', '')
+insert DataConversionFrequencyCode values ('SDE17', '', '')
+insert DataConversionFrequencyCode values ('SDE18', '', '')
+
+-- update icservicetbl set Frequency = 'Three times per week' where IEPComplSeqnum = 13833 and ServSeqNum = 31955
+
+declare @fuzzy varchar(100) ; set @fuzzy = '''Three %W%k%'' or Frequency like ''3%W%k%'''
+print @fuzzy
+--SDE04	
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE04', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency like @Fuzzy
+
+-- where isnull(del_flag,0)=0 and Frequency not like '%min%' and Frequency not like '%h%r[s]%' and  (Frequency like 'Three%W%k%' or Frequency like '3%W%k%')
+-- and iepcomplseqnum = 13833
+
+union all
+--SDE05	Four times per week
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE05', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency not like '%min%' and Frequency not like '%h%r%s%' and  (Frequency like 'Four%W%k%' or Frequency like '4%W%k%')
+union all
+--SDE06	Weekly
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE06', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency = 'Weekly'
+union all
+--SDE07	Biweekly
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE07', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency like 'Bi%weekly'
+union all
+--SDE08	Monthly
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE08', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency = 'Monthly'
+union all
+--SDE09	Quarterly
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE09', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency = 'Quarterly'
+union all
+--SDE10	One semester
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE10', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency in ('One semester', '1 semester')
+union all
+--SDE11	Semester
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE11', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency = 'Semester'
+union all
+--SDE12	Yearly
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE12', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency = 'Yearly'
+union all
+--SDE13	One time
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE13', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency in ('One time', 'Once')
+union all
+--SDE14	Two times
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE14', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency in ('Two times', 'Twice', '2x')
+union all
+--SDE15	Three times
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE15', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency in ('Three times', '3x')
+union all
+--SDE16	Four times
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE16', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency in ('Four times', '4x')
+union all
+--SDE17	Daily
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE17', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency in ('Daily', 'Every day')
+union all
+--SDE18	Consultation
+select IEPComplSeqNum, ServSeqNum, FrequencyCode = 'SDE18', Frequency
+from ICServiceTbl 
+where isnull(del_flag,0)=0 and Frequency like 'Consult%'
+
+
+
+
+
+if exists (select 1 from sys.objects where name = 'DataConversionFrequencyCodeView')
+drop view DataConversionFrequencyCodeView
+go
+
+create view DataConversionFrequencyCodeView
+as
+with ServiceCTE as
+(
+	select v.IEPComplSeqNum, 
+		v.ServSeqNum, 
+		FrequencyCode = case when v.FrequencyCode like 'SDE%' then v.FrequencyCode else NULL end, 
+		v.FrequencyDesc
+	from DataConvICServiceTbl v
+)
+ get exact matches and fuzzy matches
+select 
+	Match = 'Match 1',
+	IEPComplSeqNum = isnull(convert(varchar(10), v.IEPComplSeqNum), ''), 
+	ServSeqNum = isnull(convert(varchar(10), v.ServSeqNum), ''), 
+	OriginalFrequencyCode = v.FrequencyCode,
+	 Use original Code, if not, get code from Name match, if not get code from fuzzy match. 
+	FrequencyCode = coalesce(
+		v.FrequencyCode, 
+		case when v.FrequencyCode is null and m.SDEDesc = v.FrequencyDesc then m.SDECode else NULL end,
+		case when not (m.SDECode = isnull(v.FrequencyCode,'') or m.SDEDesc = isnull(v.FrequencyDesc,'')) and v.FrequencyDesc like m.FuzzyDesc then m.SDECode else NULL end
+		),
+	v.FrequencyDesc
+from DataConversionServiceDefCode m
+join ServiceCTE v on 
+	m.SDECode = v.FrequencyCode or 
+	m.SDEDesc = v.FrequencyDesc or
+	v.FrequencyDesc like m.FuzzyDesc
+
+union all
+
+select 
+	Match = 'Match 2',
+	convert(varchar(10), v.IEPComplSeqNum), 
+	convert(varchar(10), v.ServSeqNum), 
+	OriginalFrequencyCode = v.FrequencyCode,
+	FrequencyCode = isnull(v.FrequencyDesc, 'ZZZ'),
+	FrequencyDesc = isnull(v.FrequencyDesc, 'Not Provided')
+from DataConversionServiceDefCode m
+right join ServiceCTE v on -------------------------------- right join
+	m.SDECode = v.FrequencyCode or 
+	m.SDEDesc = v.FrequencyDesc or
+	v.FrequencyDesc like m.FuzzyDesc
+where m.SDECode is null ------ this is the trick!
+
+
+
+
+
+
+
+
+
+
+
+
+
 if exists (select 1 from sys.objects where name = 'DataConversionFrequencyCodeView')
 drop view DataConversionFrequencyCodeView
 go
