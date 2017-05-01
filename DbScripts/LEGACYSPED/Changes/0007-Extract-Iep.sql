199f4f5b6d92801185a67d3997577cf2f91411c4
georgegi <github@georgegiddens.com>
2017-05-01 17:24:23 -0400

    IEP Dates columns name and order changes.

--- DbScripts/LEGACYSPED/Changes/0007-Extract-Iep.sql
+++ DbScripts/LEGACYSPED/Changes/0007-Extract-Iep.sql
@@ -3,27 +3,28 @@ DROP TABLE LEGACYSPED.IEP_LOCAL
 GO
 
 
-CREATE TABLE LEGACYSPED.IEP_LOCAL(
-
-IepRefID	varchar(150) not null,
-StudentRefID	varchar(150) not null,
-IEPMeetDate	datetime not null,
-IEPStartDate	datetime not null,
-IEPEndDate	datetime not null,
-NextReviewDate	datetime not null,
-InitialEvaluationDate	datetime null,
-LatestEvaluationDate	datetime not null,
-NextEvaluationDate	datetime not null,
-EligibilityDate datetime null,
-ConsentForServicesDate	datetime not null,
-ConsentForEvaluationDate	datetime null,
-LREAgeGroup varchar(3) null,
-LRECode	varchar(150) not null,
-MinutesPerWeek	int not null,
-ServiceDeliveryStatement	varchar(8000) null
-
+CREATE TABLE LEGACYSPED.IEP_LOCAL (
+	IepRefID	varchar(150) not null,
+	StudentRefID	varchar(150) not null,
+	IEPMeetDate	datetime not null,
+	IEPStartDate	datetime not null,
+	IEPEndDate	datetime not null,
+	NextReviewDate	datetime not null,
+	NextEvaluationDate	datetime not null,
+	InitialEvaluationDate	datetime null,
+	LatestEvaluationDate	datetime not null,
+	InitialEligibilityDate datetime null,
+	NextEligibilityDate datetime null,
+	InitialIepDate datetime null,
+	InitialConsentForServicesDate	datetime not null,
+	InitialConsentForEvaluationDate	datetime null,
+	LREAgeGroup varchar(3) null,
+	LRECode	varchar(150) not null,
+	MinutesPerWeek	int not null,
+	ServiceDeliveryStatement	varchar(8000) null
 )
 GO
+
 Alter table LEGACYSPED.IEP_LOCAL
 add constraint PK_LEGACYSPED_IEP_LOCAL_IepRefID primary key (IepRefID)
 GO
